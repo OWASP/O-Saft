49709db11d1812d27ce89efee050c52ee829acae
EnDe <viele@public-files.de>
2013-11-28 22:46:05 +0100

    _yeast_data() implemented; documentation improved

--- o-saft-dbx.pm
+++ o-saft-dbx.pm
@@ -24,16 +24,34 @@ They must be defined as `our' in L<o-saft.pl>:
 
 =item $me   $mename   $mepath
 
+=item %data
+
 =item %cfg, i.e. trace, traceARG, traceCMD, traceKEY, verbose
+
+=item %check_cert %check_dest %check_conn %check_size %check_http
 
 =back
 
+Functions being used in L<o-saft.pl> shoudl be defined as empty stub there.
+For example:
+
+    sub _yeast_init() {}
+
+=head1 SPECIALS
+
+If you want to do special debugging, you can define proper functions here.
+They don't need to be defined in L<o-saft.pl> if they are used only here.
+In that case simply call the function in C<_yeast_init> or C<_yeast_exit>
+they are called at beginning and end of L<o-saft.pl>.
+It's just important that  L<o-saft.pl>  was called with either the I<--v>
+or any I<--trace>  option, which then loads this file automatically.
+
 =cut
 
-my  $SID    = "@(#) o-saft-dbx.pm 1.1 13/11/19 01:50:48";
+my  $SID    = "@(#) o-saft-dbx.pm 1.2 13/11/28 22:43:55";
 
 no warnings 'redefine';
-   # must be herein, as all subroutines are already defined in main
+   # must be herein, as most subroutines are already defined in main
    # warnings pragma is local to this file!
 package main;   # ensure that main:: variables are used
 
@@ -42,20 +60,25 @@ sub _yeast($) { local $\ = "\n"; print "#" . $mename . ": " . $_[0]; }
 sub _y_ARG    { local $\ = "\n"; print "#" . $mename . " ARG: " . join(" ", @_) if ($cfg{'traceARG'} > 0); }
 sub _y_CMD    { local $\ = "\n"; print "#" . $mename . " CMD: " . join(" ", @_) if ($cfg{'traceCMD'} > 0); }
 sub _yeast_init() {
+    #
+    #_yeast_data();  # uncomment to get these informations
+    #
+# yeast.pl 28.11.2013 22:29:58
     if (($cfg{'trace'} + $cfg{'verbose'}) >  0){
-        _yeast("      verbose= $cfg{'verbose'}");
-        _yeast("        trace= $cfg{'trace'}, traceARG=$cfg{'traceARG'}, traceCMD=$cfg{'traceCMD'}, traceKEY=$cfg{'traceKEY'}");
-        _yeast(" cmd->timeout= $cmd{'timeout'}");
-        _yeast(" cmd->openssl= $cmd{'openssl'}");
-        _yeast("  use_openssl= $cmd{'extopenssl'}");
+        _yeast("       verbose= $cfg{'verbose'}");
+        _yeast("         trace= $cfg{'trace'}, traceARG=$cfg{'traceARG'}, traceCMD=$cfg{'traceCMD'}, traceKEY=$cfg{'traceKEY'}");
+        _yeast("  cmd->timeout= $cmd{'timeout'}");
+        _yeast("  cmd->openssl= $cmd{'openssl'}");
+        _yeast("   use_openssl= $cmd{'extopenssl'}");
         _yeast("openssl cipher= $cmd{'extciphers'}");
-        _yeast("      use_SNI= $Net::SSLinfo::use_SNI");
-        _yeast("      targets= " . join(" ", @{$cfg{'hosts'}}));
+        _yeast("       use_SNI= $Net::SSLinfo::use_SNI");
+        _yeast("       targets= " . join(" ", @{$cfg{'hosts'}}));
         foreach $key (qw(port format legacy openssl cipher usehttp)) {
-            printf("#%s: %13s= %s\n", $mename, $key, $cfg{$key});
+            printf("#%s: %14s= %s\n", $mename, $key, $cfg{$key});
+               # cannot use _yeast() 'cause of pretty printing
         }
-        _yeast("      version= " . join(" ", @{$cfg{'version'}}));
-        _yeast("     commands= " . join(" ", @{$cfg{'do'}}));
+        _yeast("       version= " . join(" ", @{$cfg{'version'}}));
+        _yeast("      commands= " . join(" ", @{$cfg{'do'}}));
         _yeast("");
     }
 }
@@ -78,6 +101,96 @@ sub _vprintme {
     _v_print("$0 " . $VERSION);
     _v_print("$0 " . join(" ", @ARGV));
     _v_print("$0 " . sprintf("%02s.%02s.%s %02s:%02s:%02s", $mday, ($mon +1), ($year +1900), $h, $m, $s));
+}
+
+sub __data($) { (_is_command($_[0]) > 0)   ? "*" : "?"; }
+sub __short($){ defined $shorttexts{$_[0]} ? "*" : "!"; }
+sub _yeast_data() {
+    print "
+=== _yeast_data: check internal data structure ===
+
+  This function prints a simple overview of all available commands and checks.
+  The purpose is to show if for each command from  %cfg{'commands'}  a proper
+  key is defined  in  %data  and vice versa.
+  The purpose is to show if for each key in  %data  a proper command is defined
+  in  %cfg{'commands'}.
+  It also shows the keys of following hashes:
+      %data %check_cert %check_dest %check_conn %check_size %check_http
+  they should all have a key in the  %shorttexts  hash.
+";
+    my ($key, $old, $label, $value);
+    my @alias = ();     # list of potential alias commands
+    my @yeast = ();     # list of potential internal, private commands
+    printf("%20s %s   %s  %s %s %s %s %s %s\n", "key", "command", "data", " short ", "c._cert", "c._conn", "c._dest", "c._size", "c._http");
+    printf("%20s+%s+%s+%s+%s+%s+%s+%s+%s\n", "-"x20, "-"x7, "-"x7, "-"x7, "-"x7, "-"x7, "-"x7, "-"x7, "-"x7);
+    $old = "";
+    foreach $key
+            (sort {uc($a) cmp uc($b)}
+		@{$cfg{'commands'}},
+                keys %data,
+                keys %shorttexts,
+                keys %check_cert,
+                keys %check_conn,
+                keys %check_dest,
+                keys %check_size,
+                keys %check_http
+            )
+            # we use sort case-insensitively, hence the BLOCK for comparsion
+            # it also avoids the warning: sort (...) interpreted as function
+    {
+        next if ($key eq $old); # unique
+        $old = $key;
+	if ((! defined $check_cert{$key}) and
+            (! defined $check_conn{$key}) and
+            (! defined $check_dest{$key}) and
+            (! defined $check_size{$key}) and
+            (! defined $check_http{$key}) and
+            (! defined $data{$key})
+           )
+        {
+	    if (_is_member(uc($key), \@{$cfg{'COMMANDS'}}) > 0) {
+                push(@alias, $key); # seems to be an alias in uppercase
+            } else {
+                push(@yeast, $key); # probaly internal command
+            }
+            next;
+        }
+        $cmd = " ";
+	$cmd = "+" if (_is_command($key) > 0);  # command available as is
+	$cmd = "-" if (_is_member(uc($key), \@{$cfg{'COMMANDS'}}) > 0); # command available as uppercase too
+        printf("%20s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $key,
+	    $cmd,
+            (defined $data{$key})       ? __data( $key) : " ",
+            (defined $shorttexts{$key}) ?          "*"  : " ",
+            (defined $check_cert{$key}) ? __short($key) : " ",
+            (defined $check_conn{$key}) ? __short($key) : " ",
+            (defined $check_dest{$key}) ? __short($key) : " ",
+            (defined $check_size{$key}) ? __short($key) : " ",
+            (defined $check_http{$key}) ? __short($key) : " ",
+            );
+    }
+    printf("%20s+%s+%s+%s+%s+%s+%s+%s+%s\n", "-"x20, "-"x7, "-"x7, "-"x7, "-"x7, "-"x7, "-"x7, "-"x7, "-"x7);
+    print '
+    +  command (key) present
+    -  command can be upper or lower case
+    *  key present
+       key not present
+    !  key present but missing in %shorttexts (%check_* only)
+    ?  key in %data present but missing in $cfg{commands}
+
+    A shorttext should be available for each command and all data keys, except:
+        cn_nosni, ext_*, valid-*
+
+    There should be no duplicates in the C._* columns.
+
+    Please check following keys, they skipped in table above due to
+    ';
+    print "    probably lower-case aliases:\n        "  . join(" ", @alias);
+    print "    internal or summary commands:\n        " . join(" ", @yeast);
+}
+
+sub _yeast_cipher() {
+# ToDo: %ciphers %cipher_names
 }
 
 1;
