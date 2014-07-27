721ae331afe9fba0093264f9df7f9bf29d4d651d
EnDe <viele@public-files.de>
2014-07-27 16:31:29 +0200

    usr_version() and usr_pre_init() added; formal (name) changes

--- o-saft-usr.pm
+++ o-saft-usr.pm
@@ -14,13 +14,23 @@ require "o-saft-usr.pm";
 
 Defines all function for user customization.
 
+WARNING: this is not a perl module defined with `package', but uses:
+    package main;
+hence is is recommended that all variables and function use a unique
+prefix like:
+    usr_  or _usr_
+
 =head2 Functions defined herein
 
 =over 4
 
+=item usr_pre_init( )
+
+At beginning, right before initializing internal data.
+
 =item usr_pre_file( )
 
-At beginning, right before reading any  L<RC-FILE> or  L<DEBUG-FILE>
+At beginning, right after initializing internal data.
 
 =item usr_pre_args( )
 
@@ -108,33 +118,39 @@ For example:
 
 =head1 VERSION
 
-14.06.08
+Call:  usr_version()
 
 =cut
 
-my  $SID    = "@(#) o-saft-usr.pm 1.5 14/06/21 19:12:29";
+my  $usr_SID= "@(#) o-saft-usr.pm 1.6 14/07/27 16:30:50";
 
 no warnings 'redefine';
    # must be herein, as most subroutines are already defined in main
    # warnings pragma is local to this file!
 package main;   # ensure that main:: variables are used
 
-sub _dbx { _trace(join(" ", @_)); } # requires --v
+sub _usr_dbx { _trace(join(" ", @_)); } # requires --v
 
 # user functions
 # -------------------------------------
 # These functions are called in o-saft.pl
 
+sub usr_version()   { return "14.07.25"; }
+
+sub usr_pre_init()  {
+    _usr_dbx("usr_pre_init ...");
+};
+
 sub usr_pre_file()  {
-    _dbx("usr_pre_file ...");
+    _usr_dbx("usr_pre_file ...");
 };
 
 sub usr_pre_args()  {
-    _dbx("usr_pre_args ...");
+    _usr_dbx("usr_pre_args ...");
 };
 
 sub usr_pre_exec()  {
-    _dbx("usr_pre_exec ...");
+    _usr_dbx("usr_pre_exec ...");
     # All arguments and options are parsed.
     # Unknown commands are not available with _is_do() but can be
     # searched for in cfg{'done'}->{'arg_cmds'} which allows users
@@ -161,23 +177,23 @@ sub usr_pre_exec()  {
 };
 
 sub usr_pre_cipher(){
-    _dbx("usr_pre_cipher ...");
+    _usr_dbx("usr_pre_cipher ...");
 };
 
 sub usr_pre_main()  {
-    _dbx("usr_pre_main ...");
+    _usr_dbx("usr_pre_main ...");
 };
 
 sub usr_pre_host()  {
-    _dbx("usr_pre_host ...");
+    _usr_dbx("usr_pre_host ...");
 };
 
 sub usr_pre_info()  {
-    _dbx("usr_pre_info ...");
+    _usr_dbx("usr_pre_info ...");
 };
 
 sub usr_pre_open()  {
-    _dbx("usr_pre_open ...");
+    _usr_dbx("usr_pre_open ...");
     ###
     ### sample code for using your own socket
     ###
@@ -187,33 +203,33 @@ sub usr_pre_open()  {
 };
 
 sub usr_pre_cmds()  {
-    _dbx("usr_pre_cmds ...");
+    _usr_dbx("usr_pre_cmds ...");
 };
 
 sub usr_pre_data()  {
-    _dbx("usr_pre_data ...");
+    _usr_dbx("usr_pre_data ...");
 };
 
 sub usr_pre_print() {
-    _dbx("usr_pre_print ...");
+    _usr_dbx("usr_pre_print ...");
 };
 
 sub usr_pre_next()  {
-    _dbx("usr_pre_next ...");
+    _usr_dbx("usr_pre_next ...");
 };
 
 sub usr_pre_exit()  {
-    _dbx("usr_pre_exit ...");
+    _usr_dbx("usr_pre_exit ...");
 };
 
 # local functions
 # -------------------------------------
-sub _http_head()  {
+sub _usr_http_head(){
     print "X-Cite: Perl is a mess. But that's okay, because the problem space is also a mess. Larry Wall\r\n";
     print "Content-type: text/plain; charset=utf-8\r\n";
     print "\r\n";
 }
-sub _html_head()  {
+sub _usr_html_head(){
     print << "EoHTML";
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html><head>
@@ -239,7 +255,7 @@ function t(id){id.display=(id.display=='none')?'block':'none';}
  <h2>O - S a f t &#160; &#151; &#160; OWASP SSL advanced forensic tool</h2><!-- hides unwanted text before <body> tag -->
 EoHTML
 }
-sub _html_foot()  {
+sub _usr_html_foot(){
     print << "EoHTML";
  <a href="https://github.com/OWASP/O-Saft/"   target=_github >Repository</a> &nbsp;
  <a href="https://github.com/OWASP/O-Saft/blob/master/o-saft.tgz" target=_tar ><button value="" />Download (stable)</button></a><br>
@@ -249,38 +265,38 @@ sub _html_foot()  {
 EoHTML
 }
 
-sub _html_chck($) {
-    #? same as _html_cbox() but without lable and only if passed parameter start with - or +
+sub _usr_html_chck($){
+    #? same as _usr_html_cbox() but without lable and only if passed parameter start with - or +
     my $n = shift || "";
     return "" if ($n !~ m/^(-|\+)+/);
     return sprintf("<input type=checkbox name='%s' value='' >&#160;", scalar((split(/\s+/,$n))[0]));
 }
-sub _name_ankor($){
+sub _usr_name_ankor($){
     my $n = shift;
     $n =~ s/,//g;  # remove comma
     #$n =~ s/\s/_/g;# replace spaces
     return $n;
 }
-sub _html_ankor($){
+sub _usr_html_ankor($){
     #? print ankor tag for each word in given parameter
     my $n = shift;
     my $a = "";
     return sprintf("<a name=\"a%s\"></a>", $n) if ($n !~ m/^(-|\+)+/);
     #foreach $n (split(/\s+/,$n)) {
-    #    $n = _name_ankor($n);
+    #    $n = _usr_name_ankor($n);
     #    $a .= sprintf("<a name='a%s'></a>", $n);
     #}
     return sprintf("<a name=\"a%s\"></a>", $n);
     return $a;
 }
-sub _html_cbox($) { return sprintf("%8s--%-10s<input type=checkbox name=%-12s value='' >&#160;\n", "", $_[0], '"--' . $_[0] . '"'); }
-sub _html_text($) { return sprintf("%8s--%-10s<input type=text     name=%-12s size=8 >&#160;\n", "", $_[0], '"--' . $_[0] . '"'); }
-sub _html_span($) { return sprintf("%8s<span>%s</span><br>\n", "", join(", ", @{$cfg{$_[0]}})); }
-sub _html_cmd($)  { return sprintf("%9s+%-10s<input type=text     name=%-12s size=8 >&#160;\n", "", "", '"--' . $_[0] . '"'); }
+sub _usr_html_cbox($) { return sprintf("%8s--%-10s<input type=checkbox name=%-12s value='' >&#160;\n", "", $_[0], '"--' . $_[0] . '"'); }
+sub _usr_html_text($) { return sprintf("%8s--%-10s<input type=text     name=%-12s size=8 >&#160;\n", "", $_[0], '"--' . $_[0] . '"'); }
+sub _usr_html_span($) { return sprintf("%8s<span>%s</span><br>\n", "", join(", ", @{$cfg{$_[0]}})); }
+sub _usr_html_cmd($)  { return sprintf("%9s+%-10s<input type=text     name=%-12s size=8 >&#160;\n", "", "", '"--' . $_[0] . '"'); }
 
-sub _html_br()    { return sprintf("        <br>\n"); }
+sub _usr_html_br()    { return sprintf("        <br>\n"); }
 
-sub _get_html($$) {
+sub _usr_get_html($$) {
     my $anf = shift; # pattern where to start extraction
     my $end = shift; # pattern where to stop extraction
     my $cmd = "";
@@ -296,8 +312,8 @@ sub _get_html($$) {
         next if $c==1;                          # ignore comments
         next if $h==0;                          # ignore "out of scope"
         next if m/^\s*$/;                       # ignore empty lines
-        m/^=head1\s*(.*)/ && do { printf("\n<h1>%s %s</h1>\n",_html_ankor($1),$1);next;};
-        m/^=head([23])\s*(.*)/ && do { $i=$1;$i+=1;printf("%s\n<h%s>%s %s</h%s><p onclick='t(this);return false;'>\n",_html_ankor($2),$i,_html_chck($2),$2,$i);next;};
+        m/^=head1\s*(.*)/ && do { printf("\n<h1>%s %s</h1>\n",_usr_html_ankor($1),$1);next;};
+        m/^=head([23])\s*(.*)/ && do { $i=$1;$i+=1;printf("%s\n<h%s>%s %s</h%s><p onclick='t(this);return false;'>\n",_usr_html_ankor($2),$i,_usr_html_chck($2),$2,$i);next;};
         #s#B<([^>]*)>#<u>$1</u>#g;              # markup references inside help
         s#C<([^>]*)>#<span class=c >$1</span>#g;# markup examples
         s#L<([^>]*)>#"$1"#g;                    # markup other references
@@ -316,22 +332,22 @@ sub _get_html($$) {
 sub usr_printhelp() {
     #? print complete HTML page for o-saft.pl +gen-html
     #? recommended usage:   $0 --no-header --usr +gen-html
-    _dbx("usr_printhtml ...");
-    _http_head() if (grep(/^usr-cgi/, @{$cfg{'usr-args'}}) > 0);
-    _html_head();
-    _get_html('^__DATA__', '^TODO');
-    _html_foot();
+    _usr_dbx("usr_printhtml ...");
+    _usr_http_head() if (grep(/^usr-cgi/, @{$cfg{'usr-args'}}) > 0);
+    _usr_html_head();
+    _usr_get_html('^__DATA__', '^TODO');
+    _usr_html_foot();
 } # usr_printhelp
 
 sub usr_printcgi() {
     #? print complete HTML page for o-saft.pl used as CGI
     #? recommended usage:   $0 --no-header --usr +gen-cgi
     #?    o-saft.cgi?--cgi=&--usr&--no-header=&--cmd=html
-    _dbx("usr_printcgi ...");
+    _usr_dbx("usr_printcgi ...");
     my $cgi = get_usr_value('user-action') || get_usr_value('usr-action') || "/cgi-bin/o-saft.cgi"; # get action from --usr-action= or set to default
     my $key = "";
-    _http_head() if (grep(/^usr-cgi/, @{$cfg{'usr-args'}}) > 0);
-    _html_head();
+    _usr_http_head() if (grep(/^usr-cgi/, @{$cfg{'usr-args'}}) > 0);
+    _usr_html_head();
 print << "EoHTML";
  <a href="$cgi?--cgi&--help" target=_help ><button value="" />help</button></a>&#160;&#160;
  <a href="$cgi?--cgi&--help=command" target=_help ><button value="" />commands</button></a>&#160;&#160;
@@ -345,15 +361,15 @@ print << "EoHTML";
   <fieldset>
 EoHTML
 
-    print _html_text('host');
-    print _html_text('port');
+    print _usr_html_text('host');
+    print _usr_html_text('port');
 print << "EoHTML";
     <div id=a style="display:block;">
         <button class=r onclick="t(d('a'));t(d('b'));return false;">Full GUI</button><br>
 EoHTML
-    foreach $key (qw(cmd cmd cmd cmd)) { print _html_cmd($key); }
-    print _html_br();
-    print _html_span('cmd-intern');
+    foreach $key (qw(cmd cmd cmd cmd)) { print _usr_html_cmd($key); }
+    print _usr_html_br();
+    print _usr_html_span('cmd-intern');
     foreach $key (qw(sslv3 tlsv1 tlsv11 tlsv12 tlsv13 sslv2null BR
                      no-sni sni no-http http BR
                      no-dns dns no-cert BR
@@ -362,14 +378,14 @@ EoHTML
                      enabled disabled BR
                      v v trace trace traceCMD traceKEY BR
                  )) {
-        if ($key eq 'BR') { print _html_br(); next; }
-        print _html_cbox($key);
+        if ($key eq 'BR') { print _usr_html_br(); next; }
+        print _usr_html_cbox($key);
     }
-    foreach $key (qw(separator timeout legacy)) { print _html_text($key); }
-    print _html_br();
-    print _html_span('legacys');
-    print _html_text("format");
-    print _html_span('formats');
+    foreach $key (qw(separator timeout legacy)) { print _usr_html_text($key); }
+    print _usr_html_br();
+    print _usr_html_span('legacys');
+    print _usr_html_text("format");
+    print _usr_html_span('formats');
 
 ## cmd-intern:
 ## cipher check dump check_sni exec help info info--v http quick list libversion sizes s_client version quit sigkey bsi ev cipherraw cn_nosni valid-years valid-months valid-days
@@ -387,7 +403,7 @@ EoHTML
         <input type=text     name=--cmds size=55 />&#160;
 EoHTML
 
-    _get_html("^=head1\\s*COMMANDS", '^=head1\\s*LAZY');
+    _usr_get_html("^=head1\\s*COMMANDS", '^=head1\\s*LAZY');
     print << "EoHTML";
 </p>
     </div>
@@ -395,14 +411,14 @@ EoHTML
   </fieldset>
  </form>
 EoHTML
-    _html_foot();
+    _usr_html_foot();
 } # usr_printcgi
 
 sub usr_printwiki() {
     #? print documentation for o-saft.pl in mediawiki format (to be used at owasp.org)
     #? recommended usage:   $0 --no-header --usr +gen-wiki
     # ToDo: this is a simple approach!
-    _dbx("usr_printwiki ...");
+    _usr_dbx("usr_printwiki ...");
     my $key = "";
     # 1. generate wiki page header
     print "
@@ -426,7 +442,7 @@ __TOC__
     my $h = 0;
     if (open(FID, $0)) {
     while (<FID>) {
-        # following matches should be similar to those in _get_html()
+        # following matches should be similar to those in _usr_get_html()
         $h=1 if/^__DATA__/;
         next if/^__DATA__/;
         next if/^=(pod|cut|over|back|for|encoding)/;
