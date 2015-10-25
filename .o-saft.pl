044e5c1f7aa0b1f75f712824aae05b74a5ce01a2
EnDe <viele@public-files.de>
2015-10-25 18:52:40 +0100

    +npn added to --cfg_cmd=prots=

--- .o-saft.pl
+++ .o-saft.pl
@@ -143,7 +143,7 @@
 ###
 ### redefine command +protocols
 ###
---cfg_cmd=prots=hassslv2 hassslv3 hastls10 hastls11 hastls12 hastls13 session_protocol protocols https_protocols http_protocols https_svc http_svc
+--cfg_cmd=prots=hassslv2 hassslv3 hastls10 hastls11 hastls12 hastls13 npn session_protocol protocols https_protocols http_protocols https_svc http_svc
 
 ###
 ### redefine command +vulns
