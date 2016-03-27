e93ae5200bedd6148b040cd481dfa2cd2ed8c4a2
EnDe <viele@public-files.de>
2016-03-27 08:59:46 +0200

    formal change

--- o-saft.pl
+++ o-saft.pl
@@ -1225,7 +1225,7 @@ our %cmd = (
                                 # valid protocols: SMTP, IMAP, IMAP2, POP3, FTPS, LDAP, RDP, XMPP
     'slowServerDelay'   => 0,   # time to wait in seconds after a connection via proxy or before starting STARTTLS sequence
     'starttlsDelay' => 0,       # STARTTLS: time to wait in seconds (to slow down the requests)
-    'starttls_pahse'=> [],      # STARTTLS: Array for customized STARTTLS sequences
+    'starttls_phase'=> [],      # STARTTLS: Array for customized STARTTLS sequences
     'starttls_error'=> [],      # STARTTLS: Array for customized STARTTLS sequences error handling
     'enabled'       => 0,       # 1: only print enabled ciphers
     'disabled'      => 0,       # 1: only print disabled ciphers
@@ -5682,11 +5682,11 @@ while ($#argv >= 0) {
         if ($typ eq 'STARTTLSE1'){$cfg{'starttls_error[1]'}     = $arg;     $typ = 'HOST'; }
         if ($typ eq 'STARTTLSE2'){$cfg{'starttls_error[2]'}     = $arg;     $typ = 'HOST'; }
         if ($typ eq 'STARTTLSE3'){$cfg{'starttls_error[3]'}     = $arg;     $typ = 'HOST'; }
-        if ($typ eq 'STARTTLSP1'){$cfg{'starttls_pahse[1]'}     = $arg;     $typ = 'HOST'; }
-        if ($typ eq 'STARTTLSP2'){$cfg{'starttls_pahse[2]'}     = $arg;     $typ = 'HOST'; }
-        if ($typ eq 'STARTTLSP3'){$cfg{'starttls_pahse[3]'}     = $arg;     $typ = 'HOST'; }
-        if ($typ eq 'STARTTLSP4'){$cfg{'starttls_pahse[4]'}     = $arg;     $typ = 'HOST'; }
-        if ($typ eq 'STARTTLSP5'){$cfg{'starttls_pahse[5]'}     = $arg;     $typ = 'HOST'; }
+        if ($typ eq 'STARTTLSP1'){$cfg{'starttls_phase[1]'}     = $arg;     $typ = 'HOST'; }
+        if ($typ eq 'STARTTLSP2'){$cfg{'starttls_phase[2]'}     = $arg;     $typ = 'HOST'; }
+        if ($typ eq 'STARTTLSP3'){$cfg{'starttls_phase[3]'}     = $arg;     $typ = 'HOST'; }
+        if ($typ eq 'STARTTLSP4'){$cfg{'starttls_phase[4]'}     = $arg;     $typ = 'HOST'; }
+        if ($typ eq 'STARTTLSP5'){$cfg{'starttls_phase[5]'}     = $arg;     $typ = 'HOST'; }
         if ($typ eq 'PORT')     { $cfg{'port'}      = $arg;     $typ = 'HOST'; }
         #if ($typ eq 'HOST')    # not done here, but at end of loop
             #  ------+----------+------------------------------+--------------------
@@ -6558,7 +6558,7 @@ if (defined $Net::SSLhello::VERSION) {
     $Net::SSLhello::proxyport   = $cfg{'proxyport'};
     $Net::SSLhello::cipherrange = $cfg{'cipherrange'};  # not really necessary, see below
     # TODO: need to unify variables
-    @{$Net::SSLhello::starttlsPhaseArray} = @{$cfg{'starttls_pahse'}};
+    @{$Net::SSLhello::starttlsPhaseArray} = @{$cfg{'starttls_phase'}};
     push(@{$Net::SSLhello::starttlsPhaseArray}, @{$cfg{'starttls_error'}});
 }
 $cfg{'trace'} = 0 if ($cfg{'traceME'} < 0);
