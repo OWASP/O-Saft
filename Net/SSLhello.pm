90f29e65d64b000b882f219f084b15de0103824a
sslHello <torsten.gigler@owasp.org>
2016-04-02 00:49:28 +0200

    Version updated

--- Net/SSLhello.pm
+++ Net/SSLhello.pm
@@ -39,7 +39,7 @@ package Net::SSLhello;
 
 use strict;
 use constant {
-    SSLHELLO_VERSION=> '16.03.31',
+    SSLHELLO_VERSION=> '16.04.01',
     SSLHELLO        => 'O-Saft::Net::SSLhello ',
 #   SSLHELLO_SID    => '@(#) %M% %I% %E% %U%',
 };
