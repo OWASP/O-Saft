b8d23ea9b7d35b24c9c68ad39aa0cab479348990
EnDe <viele@public-files.de>
2016-04-03 23:59:55 +0200

    description and OPTIONS added

--- contrib/gen_standalone.sh
+++ contrib/gen_standalone.sh
@@ -1,19 +1,53 @@
-#!/bin/sh -x
+#!/bin/sh
 #?
 #? NAME
 #?       $0 - generate o-saft_standalone.pl
+#?
 #? SYNOPSIS
 #?       $0
+#?
+#? OPTIONS
+#?       --h     got it
+#?       --n     do not execute, just show what would be done
+#?       --v     be a bit verbose
+#?
 #? DESCRIPTION
-#?       Generate script, which coontains all module for O-Saft.
+#?       Generate script, which contains all modules for O-Saft.
+#?
+#?       NOTE: this will not generate a bulletproof standalone script!
+#?       To get a real standalone script, please see
+#?           o-saft.pl --help=INSTALL
+#?
+#?       The generated script is mainly used to check the syntax, the  variable
+#?       and i sub  declaraions.
+#?       Running the generated script may report various perl warnings
+#?           Subroutine XXXX redefined at ...
+#?           "our" variable XXXX redeclared at ...
 #?
 #? VERSION
-#?       @(#) gen_standalone.sh 1.1 16/04/03 01:41:10
+#?       @(#) gen_standalone.sh 1.2 16/04/03 23:59:47
 #?
+#? AUTHOR
+#?      02-apr-16 Achim Hoffmann
+#?
+# -----------------------------------------------------------------------------
 
 dst=o-saft_standalone.pl
 src=o-saft.pl
 try=
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+	 '-h' | '--h' | '--help')
+		ich=${0##*/}
+		\sed -ne "s/\$0/$ich/g" -e '/^#?/s/#?//p' $0
+		exit 0
+		;;
+	 '-n' | '--n') try=echo; ;;
+	 '-v' | '--v') set -x  ; ;;
+	esac
+	shift
+done
 
 o_saft="\
 	osaft.pm \
@@ -25,12 +59,17 @@ o_saft="\
 "
 
 for f in $o_saft ; do
-	$try \egrep -q 'SID.*1.1' $f \
+	\egrep -q 'SID.*1.2' $f \
 	  && \echo "**ERROR: $f wird bearbeitet; exit" \
 	  && exit 2
 done
 
-$try rm -rf $dst
+\echo "# generate $dst ..."
+\echo ""
+
+$try \rm -rf $dst
+
+[ "$try" = "echo" ] && dst=/dev/stdout
 
 (
   cat <<'EoT'
@@ -46,18 +85,24 @@ our $mename = "yeast  ";
 our (%cfg, %cmd, %data, %checks, %shorttexts, %org, %text);
 our (@dbxexe, @dbxarg, @dbxcfg);
 
+use constant {
+    # dirty hask to avoid error
+    STR_WARN  => "**WARNING: ",
+    STR_ERROR => "**ERROR: ",
+};
+
 
 EoT
 
   for f in $o_saft ; do
-	echo "# $f {"
-	$try perl -ne 'print if m(# PACKAGE {)..m(# PACKAGE })' $f
-	echo "# $f }"
-	echo ""
+	\echo "# $f {"
+	$try \perl -ne 'print if m(# PACKAGE {)..m(# PACKAGE })' $f
+	\echo "# $f }"
+	\echo ""
   done
 
-  $try cat $src
+  $try \cat $src
 
 ) > $dst
-chmod 555 $dst
+$try \chmod 555 $dst
 
