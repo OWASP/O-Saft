45e91bd5fb78e1d1d245a76ea4cdb6b202edfddd
EnDe <viele@public-files.de>
2016-04-08 12:32:45 +0200

    workaround with: no critic qw(TestingAndDebugging::RequireUseStrict)

--- o-saft-man.pm
+++ o-saft-man.pm
@@ -6,6 +6,7 @@
 
 package main;   # ensure that main:: variables are used
 
+## no critic qw(TestingAndDebugging::RequireUseStrict)
 use warnings;
 binmode(STDOUT, ":unix");
 binmode(STDERR, ":unix");
@@ -15,7 +16,7 @@ binmode(STDERR, ":unix");
 #        However, the code herein is just for our own documentation ...
 ## no critic qw(ValuesAndExpressions::ProhibitCommaSeparatedStatements)
 
-my  $man_SID= "@(#) o-saft-man.pm 1.93 16/04/08 03:30:50";
+my  $man_SID= "@(#) o-saft-man.pm 1.94 16/04/08 12:32:30";
 our $parent = (caller(0))[1] || "O-Saft";# filename of parent, O-Saft if no parent
     $parent =~ s:.*/::;
     $parent =~ s:\\:/:g;                # necessary for Windows only
