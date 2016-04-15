8c000f6c01938929dd3ce5c6a701f832a8f3920d
EnDe <viele@public-files.de>
2016-04-15 19:05:44 +0200

    descriptions for COMMNDS improved

--- o-saft-man.pm
+++ o-saft-man.pm
@@ -16,7 +16,7 @@ binmode(STDERR, ":unix");
 #        However, the code herein is just for our own documentation ...
 ## no critic qw(ValuesAndExpressions::ProhibitCommaSeparatedStatements)
 
-my  $man_SID= "@(#) o-saft-man.pm 1.102 16/04/15 18:53:10";
+my  $man_SID= "@(#) o-saft-man.pm 1.103 16/04/15 19:05:38";
 our $parent = (caller(0))[1] || "O-Saft";# filename of parent, O-Saft if no parent
     $parent =~ s:.*/::;
     $parent =~ s:\\:/:g;                # necessary for Windows only
@@ -1615,10 +1615,14 @@ COMMANDS
         A list of all available commands will be printed with:
           $0 --help=cmd
 
+        The description of all other commands will be printed with:
+          $0 --header --help=commands
+
         The summary and internal commands return requested information or the
         results of checks. These are described below.
-        The description of all other commands will be printed with:
-          $0 --help=commands
+
+        Note that some commands may be a combination of other commands, see:
+          $0 --header --help=intern
 
     Commands for information about this tool
 
