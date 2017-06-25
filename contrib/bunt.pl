395dc61d3976818064ad7e8589c88a336e6b5325
EnDe <viele@public-files.de>
2017-06-25 20:19:40 +0200

    --blind option implemented

--- contrib/bunt.pl
+++ contrib/bunt.pl
@@ -14,6 +14,7 @@
 #?      --test  simple self-testing
 #?      --line  colourize complete line
 #?      --word  colourize special words
+#?      --blind use blue instead of green
 #?      --italic colourize special words and additionally print "label texts:"
 #?               with italic characters
 #?              "label text:" is all text left of first : including :
@@ -29,7 +30,7 @@
 #       How it workd, see function  testme  below calling with  $0 --test
 #?
 #? VERSION
-#?      @(#) bunt.pl 1.8 16/06/12 10:30:38
+#?      @(#) bunt.pl 1.9 17/06/25 20:19:31
 #?
 #? AUTHOR
 #?      08-jan-16 Achim Hoffmann _at_ sicsec .dot. de
@@ -253,6 +254,10 @@ while ( $#ARGV >= 0 ) {
 	if ($arg =~ m/--line/)   { $mode='line'; $italic=0; }
 	if ($arg =~ m/--word/)   { $mode='word'; }
 	if ($arg =~ m/--italic/) { $mode='word'; $italic=1; }
+	if ($arg =~ m/--blind/)  {
+				   $map{'green'}       = $map{'blue'};
+				   $map{'light_green'} = $map{'light_blue'};
+				 }
 	if ($arg =~ m/--test/)   { testme; exit 0; }
 	if ($arg =~ m/--(\d+)/)  {
 		my $num = $1;
