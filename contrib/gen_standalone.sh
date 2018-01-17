076bdc5334a9d97fc31e8882e153ecb74fa166ab
EnDe <viele@public-files.de>
2018-01-17 09:47:26 +0100

    adapted to new files and content

--- contrib/gen_standalone.sh
+++ contrib/gen_standalone.sh
@@ -18,7 +18,7 @@
 #?       NOTE: this will not generate a bulletproof stand-alone script!
 #?
 #? VERSION
-#?       @(#) gen_standalone.sh 1.4 17/06/27 23:44:02
+#?       @(#) gen_standalone.sh 1.5 18/01/17 09:47:06
 #?
 #? AUTHOR
 #?      02-apr-16 Achim Hoffmann
@@ -27,7 +27,6 @@
 
 dst=o-saft_standalone.pl
 src=o-saft.pl
-src=yeast.pl
 try=
 sid=1
 
@@ -45,19 +44,30 @@ while [ $# -gt 0 ]; do
 	shift
 done
 
-o_saft="\
-	osaft.pm \
-	OSaft/error_handler.pm \
-	Net/SSLhello.pm \
-	Net/SSLinfo.pm \
-	o-saft-dbx.pm \
-	o-saft-usr.pm \
-	o-saft-man.pm \
+o_saft="
+	osaft.pm
+	OSaft/error_handler.pm
+	OSaft/Doc/Data.pm
+	Net/SSLhello.pm
+	Net/SSLinfo.pm
+	o-saft-dbx.pm
+	o-saft-usr.pm
+	o-saft-man.pm
 "
+
+osaft_doc="
+		OSaft/Doc/coding.txt
+		OSaft/Doc/glossary.txt
+		OSaft/Doc/help.txt
+		OSaft/Doc/links.txt
+		OSaft/Doc/rfc.txt
+"
+#	OSaft/Doc/misc \
+
 
 if [ $sid -eq 1 ]; then
 	for f in $o_saft ; do
-		\egrep -q 'SID.*1.4' $f \
+		\egrep -q 'SID.*1.5' $f \
 	  	&& \echo "**ERROR: $f changes not commited; exit" \
 	  	&& exit 2
 	done
@@ -71,15 +81,14 @@ $try \rm -rf $dst
 [ "$try" = "echo" ] && dst=/dev/stdout
 
 # general hints how to include:
-# 1.  insert into o-saft.pl at following mark:
+# 1.  extract from o-saft.pl anything before line
 ## PACKAGES
 #
 # 2. add $osaft_standalone
 #
-# 3. remove following "use"
-#use osaft;
+# 3. include osaft.pm
 #
-# 4. include text from module file enclosed in  ## PACKAGE  scope
+# 4. include text from module file enclosed in  ## PACKAGE  scope  from all modules
 #
 # 5. add rest of o-saft.pl
 
@@ -100,6 +109,13 @@ $try \rm -rf $dst
   \echo "# } # $f"
   \echo ""
 
+  ## TODO: OSaft/Doc/Data.pm
+  f=OSaft/Doc/Data.pm
+  \echo "{ # $f"
+  $try \perl -ne 'print if (m(## PACKAGE {)..m(## PACKAGE }))' $f
+  \echo "} # $f"
+  \echo ""
+
   # TODO: o-saft-usr.pm  works, but not yet perfect
   f=o-saft-usr.pm
   \echo "{ # $f"
@@ -116,11 +132,12 @@ $try \rm -rf $dst
   #\echo ""
 
   ## TODO: o-saft-man  fails to include properly
-  #f=o-saft-man.pm
-  #\echo "{ # $f"
-  #$try \perl -ne 'print if (m(## PACKAGE {)..m(## PACKAGE })) and not m(use osaft;)' $f
-  #\echo "} # $f"
-  #\echo ""
+  f=o-saft-man.pm
+  \echo "{ # $f"
+  $try \perl -ne 'print if (m(## PACKAGE {)..m(## PACKAGE })) and not m(use osaft;)' $f \
+     | \grep -v  '^use OSaft::Doc::Data'
+  \echo "} # $f"
+  \echo ""
 
   f=OSaft/error_handler.pm
   \echo "{ # $f"
@@ -132,26 +149,31 @@ $try \rm -rf $dst
   f=Net/SSLinfo.pm
   \echo "{ # $f"
   $try \perl -ne 'print if (m(## PACKAGE {)..m(## PACKAGE }))' $f
+  \echo ""
+  \echo 'my $_ssinfo_dum = $Net::SSLinfo::next_protos; # avoid Perl warning: "used only once: possible typo ..."'
   \echo "} # $f"
   \echo ""
 
   ## TODO: Net/SSLhello.pm  fails
+	# Reasons: use of STR_HINT in _hint(); need: no strict 'subs'
   #f=Net/SSLhello.pm
   #\echo "{ # $f"
-  #$try \perl -ne 'print if (m(## PACKAGE {)..m(## PACKAGE }))' $f
+  #$try \perl -ne 'print if (m(## PACKAGE {)..m(## PACKAGE }))' $f \
+  #   | \egrep -v  '^use (osaft|OSaft::error_handler)'
   #\echo "} # $f"
   #\echo ""
 
   # 5.
   \echo "package main;"
-  $try \perl -ne 'print if (not m()..m(## PACKAGES)) and not m(use osaft;)' $src
+  $try \perl -ne 'print if (not m()..m(## PACKAGES)) and not m(use osaft;)' $src \
+     | \egrep -v 'require (q.o-saft-man.pm|Net::SSLhello)'
 
 ) > $dst
 $try \chmod 555 $dst
-$try \ls -la $dst
+$try \ls    -la $dst
 \echo "# $dst generated"
 
-cat << 'EoDescription'
+cat << EoDescription
 
 	The generated stand-alone script misses following functionality:
 	* Commands
@@ -165,12 +187,19 @@ cat << 'EoDescription'
 		--trace-*
 		--exit*
 		--starttls
-	Use of any of these commands or options will result in perl compile
+	Use of any of these commands or options will result in Perl compile
 	errors like (unsorted):
 		Use of uninitialized value ...
 		Undefined subroutine ...
 		Subroutine XXXX redefined at ...
 		"our" variable XXXX redeclared at ...
+
+	However, --help and --help=* will work if following files
+	$osaft_doc
+	are located in the same directory as  $dst .
+
+	"perldoc $dst"  contains all POD of all included (module) files
+	in unsorted order.
 
 	For more details for a stand-alone script, please see:
 		o-saft.pl --help=INSTALL
