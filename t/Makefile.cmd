b05eb317a67b4ae895d53cd4bed720ec1883b509
EnDe <viele@public-files.de>
2019-10-06 20:52:54 +0200

    ET: target testcmd-c* renamed to testcmd-cmd*

--- t/Makefile.cmd
+++ t/Makefile.cmd
@@ -17,14 +17,14 @@
 #           ../Makefile  Makefile  Makefile.help  Makefile.template
 #
 #? VERSION
-#?      @(#) Makefile.cmd 1.37 19/10/03 23:18:03
+#?      @(#) Makefile.cmd 1.38 19/10/06 20:52:36
 #?
 #? AUTHOR
 #?      18-apr-18 Achim Hoffmann
 #?
 # -----------------------------------------------------------------------------
 
-_SID.cmd        = 1.37
+_SID.cmd        = 1.38
 
 _MYSELF.cmd     = t/Makefile.cmd
 ALL.includes   += $(_MYSELF.cmd)
@@ -59,14 +59,14 @@ HELP-_cmd2          = ________________________________ testing a special command
 HELP-testrun-CMD    = test specific command CMD with '$(TEST.cmd.hosts)'
 HELP-testrun-CMD.log = same as testrun-CMD but store output in '$(TEST.logdir)/'
 HELP-_cmd3          = __________________________________________ special targets _
-HELP-testcmd-c+ignored-keys = special target using commands which return random values
+HELP-testcmd-cmd+ignored-keys = special target using commands which return random values
 
 HELP.cmd            = $(_NL)\
 \# Targets can be executed individually, or a group of targets can be executed$(_NL)\
 \# by using the pattern rule  test.pattern-%  (see Makefile).$(_NL)\
 \# Examples to execute individual targets:$(_NL)\
-\#    $(MAKE_COMMAND) testcmd-c+info_localhost$(_NL)\
-\#    $(MAKE_COMMAND) testcmd-c+cipher--cipher-alpn_localhost$(_NL)\
+\#    $(MAKE_COMMAND) testcmd-cmd+info_localhost$(_NL)\
+\#    $(MAKE_COMMAND) testcmd-cmd+cipher--cipher-alpn_localhost$(_NL)\
 \#    $(MAKE_COMMAND) testrun-+cn$(_NL)\
 \# Examples to execute group of similar targets:$(_NL)\
 \#    $(MAKE_COMMAND) test.pattern-+info$(_NL)\
@@ -88,7 +88,7 @@ HELP.test.cmd.all   = # no special documentation yet
 # to avoid diffs when testing (i.e. with  *.log  targets).  To ignore output
 # for the keys, the option  --no-out= is used (alias for  --ignore-output=).
 # but keeps the command line shorter).
-# The  ignored keys are tested with the target  testcmd-c+ignored-keys_
+# The  ignored keys are tested with the target  testcmd-cmd+ignored-keys_
 _ignore-output-keys = master_key \
 		      session_id session_id_ctx \
 		      session_startdate session_starttime \
@@ -99,72 +99,72 @@ _ignore-output-cmd  = $(_ignore-output-keys:%=+%)
 # SEE Make:target name
 # SEE Make:target name prefix
 
-testcmd-c%:                     EXE.pl      = ../$(SRC.pl)
-testcmd-c%:                     TEST.init   = --trace-CLI --header
+testcmd-cmd%:                   EXE.pl      = ../$(SRC.pl)
+testcmd-cmd%:                   TEST.init   = --trace-CLI --header
 
-testcmd-c+ignored-keys_%:       TEST.args  += $(_ignore-output-cmd)
-testcmd-c+info-_%:              TEST.args  += +info               $(_ignore-output) 
-testcmd-c+info--trace-cmd_%:    TEST.args  += +info  --trace-cmd  $(_ignore-output) 
-testcmd-c+info--trace-key_%:    TEST.args  += +info  --trace-key  $(_ignore-output) 
-testcmd-c+info--trace-time_%:   TEST.args  += +info  --trace-time $(_ignore-output) 
-testcmd-c+info--trace-key-norc_%:   TEST.args  += +info   --trace-key --norc $(_ignore-output)
-testcmd-c+check_%:              TEST.args  += +check              $(_ignore-output)
-testcmd-c+check--nossltls_%:    TEST.args  += +check --nosslv2 --nosslv3 --notlsv1 --notlsv11 --notlsv12 --notlsv13 $(_ignore-output) 
+testcmd-cmd+ignored-keys_%:     TEST.args  += $(_ignore-output-cmd)
+testcmd-cmd+info-_%:            TEST.args  += +info               $(_ignore-output) 
+testcmd-cmd+info--trace-cmd_%:  TEST.args  += +info  --trace-cmd  $(_ignore-output) 
+testcmd-cmd+info--trace-key_%:  TEST.args  += +info  --trace-key  $(_ignore-output) 
+testcmd-cmd+info--trace-time_%: TEST.args  += +info  --trace-time $(_ignore-output) 
+testcmd-cmd+info--trace-key-norc_%: TEST.args  += +info   --trace-key --norc $(_ignore-output)
+testcmd-cmd+check_%:            TEST.args  += +check              $(_ignore-output)
+testcmd-cmd+check--nossltls_%:  TEST.args  += +check --nosslv2 --nosslv3 --notlsv1 --notlsv11 --notlsv12 --notlsv13 $(_ignore-output) 
     #    simulates a server not responding to ciphers
-testcmd-c+check--trace-key_%:   TEST.args  += +check --trace-key  $(_ignore-output) 
-testcmd-c+check--trace-time_%:  TEST.args  += +check --trace-time $(_ignore-output) 
-testcmd-c+check--trace-norc_%:  TEST.args  += +check --trace-cmd --trace-time --trace=2 --norc $(_ignore-output) 
-testcmd-c+check--trace-key-norc_%:  TEST.args  += +check  --trace-key --norc $(_ignore-output) 
-testcmd-c+cipher-_%:                TEST.args  += +cipher
-testcmd-c+cipher--legacy-owasp_%:   TEST.args  += +cipher --legacy=owasp
-testcmd-c+cipher--force-openssl_%:  TEST.args  += +cipher --force-openssl
-testcmd-c+cipher--cipher-openssl_%: TEST.args  += +cipher --cipher-openssl
-testcmd-c+cipher--cipher-alpn_%:    TEST.args  += +cipher --cipher-alpn
-testcmd-c+cipher--cipher-npn_%:     TEST.args  += +cipher --cipher-npn
-testcmd-c+cipher--cipher-curves_%:  TEST.args  += +cipher --cipher-curves
-#TODO: testcmd-c+cipher--cipher-npns-%:  TEST.args  += +cipher --cipher-npns=,
-#TODO: testcmd-c+cipher--cipher-npns-%:  TEST.args  += +cipher --cipher-npns=, --cipher-npns=,,
-#TODO: testcmd-c+cipher--cipher-npns-%:  TEST.args  += +cipher --cipher-npns=, --cipher-npns=ecdh_x448
-testcmd-c+cipherall_%:          TEST.args  += +cipherall
-testcmd-c+cipherraw_%:          TEST.args  += +cipherraw
-testcmd-c+cipher-dh_%:          TEST.args  += +cipher-dh
-testcmd-c+cipher-default_%:     TEST.args  += +cipher-default
-testcmd-c+ciphercheck_%:        TEST.args  += +ciphercheck
-testcmd-c+cipher--nossltls_%:   TEST.args  += +cipher --nosslv2 --nosslv3 --notlsv1 --notlsv11 --notlsv12 --notlsv13
+testcmd-cmd+check--trace-key_%: TEST.args  += +check --trace-key  $(_ignore-output) 
+testcmd-cmd+check--trace-time_%:    TEST.args  += +check --trace-time $(_ignore-output) 
+testcmd-cmd+check--trace-norc_%:    TEST.args  += +check --trace-cmd --trace-time --trace=2 --norc $(_ignore-output) 
+testcmd-cmd+check--trace-key-norc_%:  TEST.args  += +check  --trace-key --norc $(_ignore-output) 
+testcmd-cmd+cipher-_%:                TEST.args  += +cipher
+testcmd-cmd+cipher--legacy-owasp_%:   TEST.args  += +cipher --legacy=owasp
+testcmd-cmd+cipher--force-openssl_%:  TEST.args  += +cipher --force-openssl
+testcmd-cmd+cipher--cipher-openssl_%: TEST.args  += +cipher --cipher-openssl
+testcmd-cmd+cipher--cipher-alpn_%:    TEST.args  += +cipher --cipher-alpn
+testcmd-cmd+cipher--cipher-npn_%:     TEST.args  += +cipher --cipher-npn
+testcmd-cmd+cipher--cipher-curves_%:  TEST.args  += +cipher --cipher-curves
+#TODO: testcmd-cmd+cipher--cipher-npns-%:  TEST.args  += +cipher --cipher-npns=,
+#TODO: testcmd-cmd+cipher--cipher-npns-%:  TEST.args  += +cipher --cipher-npns=, --cipher-npns=,,
+#TODO: testcmd-cmd+cipher--cipher-npns-%:  TEST.args  += +cipher --cipher-npns=, --cipher-npns=ecdh_x448
+testcmd-cmd+cipherall_%:        TEST.args  += +cipherall
+testcmd-cmd+cipherraw_%:        TEST.args  += +cipherraw
+testcmd-cmd+cipher-dh_%:        TEST.args  += +cipher-dh
+testcmd-cmd+cipher-default_%:   TEST.args  += +cipher-default
+testcmd-cmd+ciphercheck_%:      TEST.args  += +ciphercheck
+testcmd-cmd+cipher--nossltls_%: TEST.args  += +cipher --nosslv2 --nosslv3 --notlsv1 --notlsv11 --notlsv12 --notlsv13
     #    simulates a server not responding to ciphers
-testcmd-c_vuln-+BEAST_%:        TEST.args  += +BEAST
-testcmd-c_vuln-+CRIME_%:        TEST.args  += +CRIME
-testcmd-c_vuln-+DROWN_%:        TEST.args  += +DROWN
-testcmd-c_vuln-+FREAK_%:        TEST.args  += +FREAK
-testcmd-c_vuln-+POODLE_%:       TEST.args  += +POODLE
-testcmd-c_vuln-+logjam_%:       TEST.args  += +logjam
-testcmd-c_vuln-+lucky13_%:      TEST.args  += +lucky13
-testcmd-c_vuln-+Sloth_%:        TEST.args  += +Sloth
-testcmd-c_vuln-+Sweet32_%:      TEST.args  += +Sweet32
-testcmd-c_summ-+bsi_%:          TEST.args  += +bsi
-testcmd-c_summ-+TR-02102+_%:    TEST.args  += +TR-02102+
-testcmd-c_summ-+EV_%:           TEST.args  += +EV
-testcmd-c_summ-+quick_%:        TEST.args  += +quick --trace-arg
-testcmd-c_summ-+ocsp_%:         TEST.args  += +ocsp
-testcmd-c_summ-+preload_%:      TEST.args  += +preload
-testcmd-c_summ-+protocols_%:    TEST.args  += +protocols
-testcmd-c_summ-+fingerprints_%: TEST.args  += +fingerprints
-testcmd-c_summ-+sizes_%:        TEST.args  += +sizes
-testcmd-c_summ-+pfs_%:          TEST.args  += +pfs
-testcmd-c_summ-+sni_%:          TEST.args  += +sni
-testcmd-c_summ-+vulns_%:        TEST.args  += +vulns
-testcmd-c_summ-+http_%:         TEST.args  += +http  --ignore-output=sts_expired
-testcmd-c_summ-+hsts_%:         TEST.args  += +hsts  --ignore-output=sts_expired
-testcmd-c_summ-+sts_%:          TEST.args  += +sts   --ignore-output=sts_expired
+testcmd-cmd_vuln-+BEAST_%:      TEST.args  += +BEAST
+testcmd-cmd_vuln-+CRIME_%:      TEST.args  += +CRIME
+testcmd-cmd_vuln-+DROWN_%:      TEST.args  += +DROWN
+testcmd-cmd_vuln-+FREAK_%:      TEST.args  += +FREAK
+testcmd-cmd_vuln-+POODLE_%:     TEST.args  += +POODLE
+testcmd-cmd_vuln-+logjam_%:     TEST.args  += +logjam
+testcmd-cmd_vuln-+lucky13_%:    TEST.args  += +lucky13
+testcmd-cmd_vuln-+Sloth_%:      TEST.args  += +Sloth
+testcmd-cmd_vuln-+Sweet32_%:    TEST.args  += +Sweet32
+testcmd-cmd_summ-+bsi_%:        TEST.args  += +bsi
+testcmd-cmd_summ-+TR-02102+_%:  TEST.args  += +TR-02102+
+testcmd-cmd_summ-+EV_%:         TEST.args  += +EV
+testcmd-cmd_summ-+quick_%:      TEST.args  += +quick --trace-arg
+testcmd-cmd_summ-+ocsp_%:       TEST.args  += +ocsp
+testcmd-cmd_summ-+preload_%:    TEST.args  += +preload
+testcmd-cmd_summ-+protocols_%:  TEST.args  += +protocols
+testcmd-cmd_summ-+fingerprints_%: TEST.args  += +fingerprints
+testcmd-cmd_summ-+sizes_%:      TEST.args  += +sizes
+testcmd-cmd_summ-+pfs_%:        TEST.args  += +pfs
+testcmd-cmd_summ-+sni_%:        TEST.args  += +sni
+testcmd-cmd_summ-+vulns_%:      TEST.args  += +vulns
+testcmd-cmd_summ-+http_%:       TEST.args  += +http  --ignore-output=sts_expired
+testcmd-cmd_summ-+hsts_%:       TEST.args  += +hsts  --ignore-output=sts_expired
+testcmd-cmd_summ-+sts_%:        TEST.args  += +sts   --ignore-output=sts_expired
     # +sts_expired may return current timestamp, hence output ignored to avoid diffs
 
-test.cmd.log-compare:   TEST.target_prefix  = testcmd-c
-test.cmd.log-move:      TEST.target_prefix  = testcmd-c
-test.cmd.log:           TEST.target_prefix  = testcmd-c
+test.cmd.log-compare:   TEST.target_prefix  = testcmd-cmd
+test.cmd.log-move:      TEST.target_prefix  = testcmd-cmd
+test.cmd.log:           TEST.target_prefix  = testcmd-cmd
 
 # SEE Make:target matching
 # NOTE: no sort because we want the sequence of target definitions above.
-ALL.testcmd     = $(shell awk -F% '($$1 ~ /^testcmd-c./){arr[$$1]=1}$(_AWK_print_arr_END)' $(_MYSELF.cmd))
+ALL.testcmd     = $(shell awk -F% '($$1 ~ /^testcmd-cmd./){arr[$$1]=1}$(_AWK_print_arr_END)' $(_MYSELF.cmd))
 ALL.test.cmd    = $(foreach host,$(TEST.cmd.hosts),$(ALL.testcmd:%=%$(host)))
 ALL.test.cmd.log  += $(ALL.test.cmd:%=%.log)
 
@@ -179,7 +179,7 @@ test.cmd.log:   $(ALL.test.cmd.log) test.log-compare-hint
 # NOTE: testrun-+%  and not  testrun+%  is used to avoid double definition of
 # the pattern rule (problem in GNU Make), this restricts the usage to pattern
 # starting with  + , unfortunately.
-# EXE.pl  and  TEST.init  will be inherited from  testcmd-c% .
+# EXE.pl  and  TEST.init  will be inherited from  testcmd-cmd% .
 testrun-+%:     TEST.args  += $(TEST.cmd.hosts)
 testrun-%: testcmd-%
 	@$(TRACE.target)
