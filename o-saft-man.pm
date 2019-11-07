5a1d90f9dc2ee51ad18b46e972fa7581aa914c20
EnDe <viele@public-files.de>
2019-11-07 23:58:50 +0100

    EF: JavaScript function schema_is_file() added

--- o-saft-man.pm
+++ o-saft-man.pm
@@ -62,7 +62,7 @@ BEGIN {     # SEE Perl:BEGIN perlcritic
 use osaft;
 use OSaft::Doc::Data;
 
-my  $SID_man= "@(#) o-saft-man.pm 1.291 19/11/07 23:48:38";
+my  $SID_man= "@(#) o-saft-man.pm 1.292 19/11/07 23:58:44";
 my  $parent = (caller(0))[1] || "O-Saft";# filename of parent, O-Saft if no parent
     $parent =~ s:.*/::;
     $parent =~ s:\\:/:g;                # necessary for Windows only
@@ -108,7 +108,7 @@ sub _man_get_title  { return 'O - S a f t  --  OWASP - SSL advanced forensic too
 sub _man_get_version{
     # ugly, but avoids global variable or passing as argument
     no strict; ## no critic qw(TestingAndDebugging::ProhibitNoStrict)
-    my $v = '1.291'; $v = STR_VERSION if (defined STR_VERSION);
+    my $v = '1.292'; $v = STR_VERSION if (defined STR_VERSION);
     return $v;
 } # _man_get_version
 
@@ -279,9 +279,13 @@ function osaft_handler(from,to){
         }
         return false;
 }
+function schema_is_file(){
+        if (/^file:/.test(location.protocol)===true) { return true; }
+        return false;
+}
 function toggle_handler(){
 // toggle display of "schema" button
-        if (/^file:/.test(location.protocol)===true) { return; }
+        if (schema_is_file()===true) { return; }
         toggle_display(d("schema"));
         return;
 }
@@ -1662,7 +1666,7 @@ In a perfect world it would be extracted from there (or vice versa).
 
 =head1 VERSION
 
-1.291 2019/11/07
+1.292 2019/11/07
 
 =head1 AUTHOR
 
