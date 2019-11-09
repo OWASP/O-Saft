1acd4678e686d1075e4ed66709f898f09cf64c0c
EnDe <viele@public-files.de>
2019-11-09 09:26:00 +0100

    EF: call o-saft.cgi instead of o-saft.pl

--- contrib/o-saft.php
+++ contrib/o-saft.php
@@ -1,12 +1,11 @@
 <?php
 #?
 #? NAME
-#?      $0 - simple wrapper for o-saft.pl
+#?      $0 - simple wrapper for o-saft.cgi
 #?
 #? WARNING ####################################################################
 #?      This is not tamper-proof code.                                       ##
 #?      It passes all received parameters unmodified "as is" to the shell.   ##
-#?      Just a few known dangerous characters are removed.                   ##
 #?      Hence this script may be subject to code injections.                 ##
 #?                                                                           ##
 #?                          You have been warned!                            ##
@@ -14,35 +13,40 @@
 #? ############################################################################
 #?
 #? DESCRIPTION
-#?      PHP wrapper to call  o-saft.pl.  All arguments are passed thru.
-#?      The purpose is to support  o-saft.pl  on web servers which have not
-#?      enabled perl as CGI, or if it is not possible to configure a proper
+#?      PHP wrapper to call  o-saft.cgi.  All arguments are passed thru.
+#?      The purpose is to support  o-saft.cgi  on web servers which have not
+#?      enabled perl as  CGI, or if it is not possible to configure a proper
 #?      handler for perl.
 #?
-#? LIMITATIONS
-#?      The special characters  ; & ` > ! | $ <  in parameters are silently
-#?      removed. This is a simple, but not perfect attempt to inhibit shell
-#?      injections.
-#?
 #? VERSION
-#?      @(#) o-saft.php 1.3 19/11/09 01:04:20
+#?      @(#) o-saft.php 1.4 19/11/09 09:25:46
 #?
 #? AUTHOR
 #?      17-feb-17 Achim Hoffmann
 # -----------------------------------------------------------------------------
 
+if (empty($_SERVER['QUERY_STRING'])) {
+    header("HTTP/1.1 406 Not Acceptable");
+    exit(2);
+}
+# NOTE: cd to directory which contains RC-file .o-saft.pl also
+$dir = "../cgi-bin";                            # where to find o-saft.[cgi|pl]
+passthru("cd ../cgi-bin;./o-saft.cgi", $err);   # pass QUERY_STRING as is
+exit(0);
+
+
+#_________________________________________ alternate methods, NOT RECOMMENDED _
+
 header('Content-Type: text/plain');
-$qs = "";
+$qs  = "";
 if (isset($_SERVER['QUERY_STRING'])) {
     $qs = join(' ', preg_split('/&/', $_SERVER['QUERY_STRING']));
 }
-$qs = preg_replace('/[;&`>!|$<]/', '', $qs, -1);# remove dangerous characters
-#dbx# echo("/cgi-bin/o-saft.pl --cgi $qs \n");
-#passthru("/cgi-bin/o-saft.pl --cgi $qs", $err);
-passthru("cd ../cgi-bin;./o-saft.pl $qs", $err);
-        # --cgi avoids messages like:  "=== reading: ..."
-        # add more options as needed
+
+$qs  = preg_replace('/[;&`>!|$<]/', '', $qs, -1);# remove just a few dangerous characters
+#dbx# echo("cd $dir;./o-saft.pl --cgi $qs \n");
+passthru(  "cd $dir;./o-saft.pl --cgi $qs", $err);# pass QUERY_STRING on command line
 #dbx# echo "# ERROR: $err\n";
-exit;
+exit(0);
 
 ?>
