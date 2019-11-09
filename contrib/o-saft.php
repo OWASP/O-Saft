d7f51da711efeb3c7b76c6595a68961272b2c3a8
EnDe <viele@public-files.de>
2019-11-09 11:50:00 +0100

    EF: improve: search for script to be called (no longer hardcoded)

--- contrib/o-saft.php
+++ contrib/o-saft.php
@@ -16,37 +16,58 @@
 #?      PHP wrapper to call  o-saft.cgi.  All arguments are passed thru.
 #?      The purpose is to support  o-saft.cgi  on web servers which have not
 #?      enabled perl as  CGI, or if it is not possible to configure a proper
-#?      handler for perl.
+#?      handler for perl but allows PHP.
+#?      o-saft.cgi or o-saft.pl  will be search for in various paths related
+#?      to $_SERVER['SCRIPT_FILENAME'].
 #?
 #? VERSION
-#?      @(#) o-saft.php 1.4 19/11/09 09:25:46
+#?      @(#) o-saft.php 1.5 19/11/09 11:49:52
 #?
 #? AUTHOR
 #?      17-feb-17 Achim Hoffmann
 # -----------------------------------------------------------------------------
 
+function get_exe($base, $file, $dirs) {
+    $exe = $file;   # fallback
+    foreach ($dirs as $dir) {
+        #$exe = realpath(os.path.join($base, $dir, $file));
+            # NOTE: os.path not used to avoid include of os
+        $exe = realpath(join(DIRECTORY_SEPARATOR, [$base, $dir, $file]));
+        if (! empty($exe)) { break; }   # got it
+    }
+    return $exe;
+}
+
+# list of directories where to search for o-saft.[cgi|pl] ; first one wins
+# NOTE: */O-Saft and ../ used for easy testing in development environment,
+#       can/should be removed in production
+$dirs = array('.', 'cgi-bin', '../cgi-bin', 'O-Saft', 'cgi-bin/O-Saft', '../cgi-bin/O-Saft', '..');
+$path = pathinfo( $_SERVER['SCRIPT_FILENAME']); # analyze myself
+
 if (empty($_SERVER['QUERY_STRING'])) {
     header("HTTP/1.1 406 Not Acceptable");
     exit(2);
 }
-# NOTE: cd to directory which contains RC-file .o-saft.pl also
-$dir = "../cgi-bin";                            # where to find o-saft.[cgi|pl]
-passthru("cd ../cgi-bin;./o-saft.cgi", $err);   # pass QUERY_STRING as is
-exit(0);
 
+$cgi  = get_exe($path['dirname'], 'o-saft.cgi', $dirs);
+$path = pathinfo($cgi);     # need to start in directory so that .o-saft.pl is used
+$call = join(' ', ['cd', $path['dirname'], ';', $path['basename'] ]);
+passthru("$call", $err);
+exit(0);
 
 #_________________________________________ alternate methods, NOT RECOMMENDED _
 
 header('Content-Type: text/plain');
-$qs  = "";
+$qs   = "";
 if (isset($_SERVER['QUERY_STRING'])) {
-    $qs = join(' ', preg_split('/&/', $_SERVER['QUERY_STRING']));
+  $qs = join(' ', preg_split('/&/', $_SERVER['QUERY_STRING']));
 }
-
-$qs  = preg_replace('/[;&`>!|$<]/', '', $qs, -1);# remove just a few dangerous characters
-#dbx# echo("cd $dir;./o-saft.pl --cgi $qs \n");
-passthru(  "cd $dir;./o-saft.pl --cgi $qs", $err);# pass QUERY_STRING on command line
-#dbx# echo "# ERROR: $err\n";
+$qs   = preg_replace('/[;&`>!|$<]/', '', $qs, -1);# remove just a few dangerous characters
+$exe  = get_exe($path['dirname'], 'o-saft.pl', $dirs);
+$call = join(' ', ['cd', $path['dirname'], ';', $path['basename'], '--cgi', $qs]);
+#dbx# echo("call=$call\n");
+passthru(  "$call", $err);  # pass QUERY_STRING on command line
+#dbx# echo "# ERROR=$err\n";
 exit(0);
 
 ?>
