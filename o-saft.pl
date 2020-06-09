6f1f2cde6eee1271b7cec2187f8ec1497f4729da
EnDe <viele@public-files.de>
2020-06-10 00:24:32 +0200

    ED: --h now different to --help; usage adapted

--- o-saft.pl
+++ o-saft.pl
@@ -65,8 +65,8 @@ use constant { ## no critic qw(ValuesAndExpressions::ProhibitConstantPragma)
     # NOTE: use Readonly instead of constant is not possible, because constants
     #       are used  for example in the  BEGIN section.  Constants can be used
     #       there but not Readonly variables. Hence  "no critic"  must be used.
-    SID         => "@(#) yeast.pl 1.993 20/06/05 00:00:56",
-    STR_VERSION => "19.12.27",          # <== our official version number
+    SID         => "@(#) yeast.pl 1.994 20/06/10 00:24:20",
+    STR_VERSION => "20.06.06",          # <== our official version number
 };
 use autouse 'Data::Dumper' => qw(Dumper);
 
@@ -7317,7 +7317,7 @@ sub printopenssl        {
 } # printopenssl
 
 sub printusage_exit     {
-    #? print simple usage
+    #? print simple usage, first line with passed text
     my @txt = @_;
     local $\ = "\n";
     print STR_USAGE, @txt;
@@ -7326,6 +7326,7 @@ sub printusage_exit     {
   $cfg{'me'} +check    your.tld
   $cfg{'me'} +cipher   your.tld
 # for more help use:
+  $cfg{'me'} --h
   $cfg{'me'} --help
     ";
     exit 2;
@@ -7671,8 +7672,9 @@ while ($#argv >= 0) {
 
     # all options starting with  --h or --help or +help  are not handled herein
     _y_ARG("opt_--h? $arg");
-    if ($arg =~ /^(?:--|\+)h(?:elp)?$/)          { $arg = "--help=NAME"; }# --h  or --help
-    if ($arg =~ /^\+(abbr|abk|glossar|todo)$/i)  { $arg = "--help=$1"; }  # for historic reason
+    if ($arg =~ /^--h$/)                        { $arg = "--help=help_cmds"; }# --h  is special
+    if ($arg =~ /^(?:--|\+)help$/)              { $arg = "--help=NAME"; }   # --help
+    if ($arg =~ /^\+(abbr|abk|glossar|todo)$/i) { $arg = "--help=$1"; }     # for historic reason
     # get matching string right of =
     if ($arg =~ /^(?:--|\+)help=?(.*)?$/) {
         # we allow:  --help=SOMETHING  or  +help=SOMETHING
