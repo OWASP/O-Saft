886f83e6331319a77b8cf5c3e5a015ee17c0021f
EnDe <viele@public-files.de>
2022-10-07 22:30:22 +0200

    EF: right align hex code for ciphers-html

--- o-saft-man.pm
+++ o-saft-man.pm
@@ -57,7 +57,7 @@ use osaft;
 use OSaft::Doc::Data;
 use OSaft::Ciphers; # required if called standalone only
 
-my  $SID_man= "@(#) o-saft-man.pm 2.63 22/10/07 22:00:56";
+my  $SID_man= "@(#) o-saft-man.pm 2.64 22/10/07 22:30:18";
 my  $parent = (caller(0))[1] || "O-Saft";# filename of parent, O-Saft if no parent
     $parent =~ s:.*/::;
     $parent =~ s:\\:/:g;                # necessary for Windows only
@@ -643,7 +643,7 @@ sub _man_usr_value  {
 sub _man_get_version{
     # ugly, but avoids global variable elsewhere or passing as argument
     no strict; ## no critic qw(TestingAndDebugging::ProhibitNoStrict)
-    my $v = '2.63'; $v = _VERSION() if (defined &_VERSION);
+    my $v = '2.64'; $v = _VERSION() if (defined &_VERSION);
     return $v;
 } # _man_get_version
 
@@ -1797,6 +1797,7 @@ table       {display: none;     }
 table th    {background:#aaa;   }
 tbody tr:nth-child(even){background:#fff;    }
 tbody tr:nth-child(odd) {background:#eee;    }
+tbody td:first-child    {text-align:right;   }
 tbody td    {width: 5em; }
 thead       {position: sticky; top:3em; }
 </style>
@@ -2496,7 +2497,7 @@ In a perfect world it would be extracted from there (or vice versa).
 
 =head1 VERSION
 
-2.63 2022/10/07
+2.64 2022/10/07
 
 =head1 AUTHOR
 
