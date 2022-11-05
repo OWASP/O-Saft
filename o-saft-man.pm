0c4d868c32af73f4314c0656fc7ac17ef755e1bd
EnDe <viele@public-files.de>
2022-11-05 13:22:33 +0100

    EF: keep perlcritic happy

--- o-saft-man.pm
+++ o-saft-man.pm
@@ -28,6 +28,8 @@ package main;   # ensure that main:: variables are used
 ## no critic qw(Variables::ProhibitPunctuationVar)
 #        We want to use $\ $0 etc.
 
+## no critic qw(Variables::ProhibitPackageVars)
+
 ## no critic qw(ControlStructures::ProhibitPostfixControls  Modules::RequireVersionVar)
 ## no critic qw(RegularExpressions::RequireDotMatchAnything RegularExpressions::RequireLineBoundaryMatching)
 ## no critic qw(ValuesAndExpressions::ProhibitEmptyQuotes   RegularExpressions::ProhibitFixedStringMatches)
@@ -57,7 +59,7 @@ use osaft;
 use OSaft::Doc::Data;
 use OSaft::Ciphers; # required if called standalone only
 
-my  $SID_man= "@(#) o-saft-man.pm 2.82 22/11/04 18:39:33";
+my  $SID_man= "@(#) o-saft-man.pm 2.83 22/11/05 13:22:21";
 my  $parent = (caller(0))[1] || "O-Saft";# filename of parent, O-Saft if no parent
     $parent =~ s:.*/::;
     $parent =~ s:\\:/:g;                # necessary for Windows only
@@ -780,7 +782,7 @@ sub _man_usr_value  {
 sub _man_get_version {
     # ugly, but avoids global variable elsewhere or passing as argument
     no strict; ## no critic qw(TestingAndDebugging::ProhibitNoStrict)
-    my $v = '2.82'; $v = _VERSION() if (defined &_VERSION);
+    my $v = '2.83'; $v = _VERSION() if (defined &_VERSION);
     return $v;
 } # _man_get_version
 
@@ -2623,7 +2625,7 @@ In a perfect world it would be extracted from there (or vice versa).
 
 =head1 VERSION
 
-2.82 2022/11/04
+2.83 2022/11/05
 
 
 =head1 AUTHOR
