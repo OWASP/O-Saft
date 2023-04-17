8a84a5667fdb13d012a4764b3ceea44b4229d622
EnDe <viele@public-files.de>
2023-04-17 22:53:14 +0200

    EF: options and aguments added; documentation improved

--- t/gen-graph-sub-calls.sh
+++ t/gen-graph-sub-calls.sh
@@ -5,6 +5,7 @@
 #?
 #? SYNOSYS
 #?      $0
+#?      $0 file ...
 #?
 #? DESCRIPTION
 #?      Generates a graph for all subs and their call from given perl file.
@@ -13,49 +14,103 @@
 #?      Generates a complete graph and a simpler one where common verbose and
 #?      trace subs are omitted.
 #?
+#?      Builds a graph of all found subs in following files:
+#?	    o-saft_graph-sub-call-full.*
+#?	    o-saft_graph-sub-call-simple.*
+#?      in following formats:   DOT  graph  PDF  SVG  TCL  VCG
+#?
+#?      make's  e-ALL.pm  target will be used to retrieved all source files.
+#?
+#? OPTIONS
+#?      --help  - WYSIWYG
+#?      --v     - verbose output
+#?      -x --x  - debug with shell's "set -x'
+#?      --list  - list used source files
+#?      --dir=D - generate file in directory D ; default: .
+#?
 #? LIMITATIONS
 #?      The script generates multiple files at once. The main reason is, that
 #?      the output of the initial generator graph.pl needs to be modified and
-#?      and then filtered before converted to other formats.
+#?      then filtered before converted to other formats.
 #?      Therefore use of this script may not fit into the  general concept of
-#?      make which assumes one result (file) for each target.
+#?      make which expects one result (file) for each target.
 #?
 #? VERSION
-#?      @(#)  1.1 23/04/17 19:54:20
+#?      @(#)  1.2 23/04/17 22:52:55
 #?
 #? AUTHOR
 #?      13-mar23 Achim Hoffmann
 #?
 #------------------------------------------------------------------------------
 
-#make# ALL.SRC.perl    = $(SRC.pl) $(ALL.pm) $(CHK.pl)
+#_____________________________________________________________________________
+#____________________________________________________________ configuration __|
 
-dir=.
-ALL_src="`(cd .. && \make e-ALL.pm)` o-saft.pl"
+# quick tests first
 
-out_full=$dir/graph-sub-calls-full_o-saft.pl
-out_simple=$dir/graph-sub-calls-simple_o-saft.pl
-
-box2ellipse="s/shape=box,/shape=ellipse,/"  # default shape should be ellipse
-
-if ! \command -v graph-easy 2>/dev/null ; then
-	\echo "**ERROR: 'graph-easy' fehlt; Abbruch"
+if ! \command -v graph-easy 2>&1 >/dev/null ; then
+	\echo "**ERROR: 'graph-easy' missing; exit"
 	exit 2
 fi
 dot=`\command -v dot 2>/dev/null`
-[ -z "$dot" ] && \echo "**ERROR: 'dot' fehlt; .pdf und .png wird nicht erzeugt"
+[ -z "$dot" ] && \echo "**ERROR: 'dot' missing; .pdf and .png not generated"
+
+try=
+ich=${0##*/}
+dir=.           # directory for generated files
+out=o-saft_graph-sub-call
+optv=
+ALL_src="`\make e-ALL.pm)` o-saft.pl"
+
+box2ellipse="s/shape=box,/shape=ellipse,/"  # default shape should be ellipse
+
+#_____________________________________________________________________________
+#________________________________________________________________ functions __|
+
+_vprint() {
+	[ -n "$optv" ] && \echo "# $@ ..." >&2
+	return
+} # _vprint
+
+#_____________________________________________________________________________
+#_____________________________________________________________________ main __|
+
+args=
+while [ $# -gt 0 ]; do
+	case "$1" in
+	 -h | --h | --help | '-?' | '/?')
+		\sed -ne "s/\$0/$ich/g" -e '/^#?/s/#?//p' $0
+		exit 0
+		;;
+	 -n | --n) try=echo      ; ;;
+	 -v | --v) optv=--v      ; ;;
+	 -x | --x) set -x        ; ;;
+	 --list)   \echo $ALL_src; exit 0 ;;
+	 --dir=*)  dir="`expr "$1" ':' '--dir=\(.*\)'`" ; ;;
+	*)      args="${args} $1"; ;;
+	esac
+	shift
+done
+
+if [ -n "$args" ]; then
+	ALL_src=$args
+fi
+
+out_full=$dir/$out-full
+out_simple=$dir/$out-simple
+
+_vprint "ALL_src=$ALL_src ..."
 
 # Processing all files at once with graph.pl would generate a huge graph with
 # only 3-4 columns. Hence each file is processed alone, and all output used
 # together in one file.
 #dbx# (cd .. && t/graph.pl $ALL_src ) > $out.graph.orig
 (
-  for src in o-saft.pl ; do
+  for src in $ALL_src ; do
     \echo "  ( $src"
     \echo "   [$src::] {shape:rect;}"
 
-    (cd .. && t/graph.pl $src ) \
-    | \gawk '
+    t/graph.pl $src | \gawk '
 	# input looks like:
 	#       digraph mygraph {
 	#       IO__Handle__read -> croak;
@@ -115,17 +170,20 @@ dot=`\command -v dot 2>/dev/null`
 \sed -e /_y_CMD/d -e '/_warn\]/d' -e /_trace/d -e /_v_print/d \
 	$out_full.graph > $out_simple.graph
 
-for out in $out_full $out_simple ; do
+_vprint "--------------------------------------------- generate graph for sub #}"
+for _out in $out_full $out_simple ; do
 	#\dot $out.graph > $out.dot             # convert DOT
-	\graph-easy  $out.graph --dot | \sed -e "$box2ellipse" > $out.dot
-	\graph-easy  $out.graph --vcg                          > $out.vcg
+	\graph-easy  $_out.graph --dot | \sed -e "$box2ellipse" > $_out.dot
+	\graph-easy  $_out.graph --vcg                          > $_out.vcg
 	if [ -n "$dot" ]; then
-		\dot $out.dot   -Tpdf                          > $out.pdf
-		#\dot$out.dot   -Tpng                          > $out.png # huge file
-		\dot $out.dot   -Tsvg                          > $out.svg
-		\echo "# evince $out.pdf"
-		\echo "# eog    $out.png"
+		\dot $_out.dot   -Tpdf                          > $_out.pdf
+		#\dot$_out.dot   -Tpng                          > $_out.png # huge file
+		\dot $_out.dot   -Tsvg                          > $_out.svg
+		_vprint "# evince $_out.pdf"
+		_vprint "# eog    $_out.png"
 	fi
-	\echo "# xdot   $out.dot"
-	done
+	_vprint "# xdot   $_out.dot"
+done
+[ -n "$optv" ] && \ls -l $dir/$out*
+_vprint "---------------------------------------------------------------------#}"
 exit
