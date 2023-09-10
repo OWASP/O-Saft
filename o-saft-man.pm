6f05c21464e6689950f0cb4d0d79ace9c4b94aae
EnDe <viele@public-files.de>
2023-09-10 11:58:57 +0200

    ED: --help=developer added

--- o-saft-man.pm
+++ o-saft-man.pm
@@ -59,7 +59,7 @@ use osaft;
 use OSaft::Doc::Data;
 use OSaft::Ciphers; # required if called standalone only
 
-my  $SID_man= "@(#) o-saft-man.pm 2.97 23/09/10 11:50:33";
+my  $SID_man= "@(#) o-saft-man.pm 2.98 23/09/10 11:58:50";
 my  $parent = (caller(0))[1] || "O-Saft";# filename of parent, O-Saft if no parent
     $parent =~ s:.*/::;
     $parent =~ s:\\:/:g;                # necessary for Windows only
@@ -793,7 +793,7 @@ sub _man_usr_value  {
 sub _man_get_version {
     # ugly, but avoids global variable elsewhere or passing as argument
     no strict; ## no critic qw(TestingAndDebugging::ProhibitNoStrict)
-    my $v = '2.97'; $v = _VERSION() if (defined &_VERSION);
+    my $v = '2.98'; $v = _VERSION() if (defined &_VERSION);
     return $v;
 } # _man_get_version
 
@@ -2449,6 +2449,9 @@ sub printhelp       {   ## no critic qw(Subroutines::ProhibitExcessComplexity)
         #$txt = _man_squeeze(undef, "@txt"); # TODO: does not work well here
         $txt = join("", @txt);
     }
+    if ($hlp =~ m/^(devel|developer|development)$/i) { # print developer description
+        $txt = join("", OSaft::Doc::Data::get("devel.txt", $parent, $version));
+    }
     if (not $txt)               { # nothing matched so far, print special section from help
         _man_dbx("printhelp: " . uc($hlp));
         $txt = man_help(uc($hlp))   if ($hlp !~ m/^[+-]-?/);    # bare words only
@@ -2633,6 +2636,10 @@ on the $format parameter, which is a literal string, as follows:
 
 =item * Program.Code  -> description of coding style, conventions, etc.
 
+=item * Developer
+
+=item * Development   -> description for developers
+
 =back
 
 If any other string is used,  'printhelp()'  extracts just the section of
@@ -2647,7 +2654,7 @@ In a perfect world it would be extracted from there (or vice versa).
 
 =head1 VERSION
 
-2.97 2023/09/10
+2.98 2023/09/10
 
 
 =head1 AUTHOR
