a2138f21d5b24b0dac1287fc0e0c2c9e0137f3e7
EnDe <viele@public-files.de>
2023-09-10 11:50:41 +0200

    EF: alias --help=coding for --alias=program.code

--- o-saft-man.pm
+++ o-saft-man.pm
@@ -59,7 +59,7 @@ use osaft;
 use OSaft::Doc::Data;
 use OSaft::Ciphers; # required if called standalone only
 
-my  $SID_man= "@(#) o-saft-man.pm 2.96 23/05/07 09:29:25";
+my  $SID_man= "@(#) o-saft-man.pm 2.97 23/09/10 11:50:33";
 my  $parent = (caller(0))[1] || "O-Saft";# filename of parent, O-Saft if no parent
     $parent =~ s:.*/::;
     $parent =~ s:\\:/:g;                # necessary for Windows only
@@ -793,7 +793,7 @@ sub _man_usr_value  {
 sub _man_get_version {
     # ugly, but avoids global variable elsewhere or passing as argument
     no strict; ## no critic qw(TestingAndDebugging::ProhibitNoStrict)
-    my $v = '2.96'; $v = _VERSION() if (defined &_VERSION);
+    my $v = '2.97'; $v = _VERSION() if (defined &_VERSION);
     return $v;
 } # _man_get_version
 
@@ -2444,7 +2444,7 @@ sub printhelp       {   ## no critic qw(Subroutines::ProhibitExcessComplexity)
         #$txt = _man_squeeze(undef, "@txt"); # TODO: does not work well here
         $txt = join("", @txt);
     }
-    if ($hlp =~ m/^Program.?Code$/i) { # print Program Code description
+    if ($hlp =~ m/^(coding|Program.?Code)$/i) { # print Program Code description
         my @txt = OSaft::Doc::Data::get("coding.txt", $parent, $version);
         #$txt = _man_squeeze(undef, "@txt"); # TODO: does not work well here
         $txt = join("", @txt);
@@ -2647,7 +2647,7 @@ In a perfect world it would be extracted from there (or vice versa).
 
 =head1 VERSION
 
-2.96 2023/05/07
+2.97 2023/09/10
 
 
 =head1 AUTHOR
