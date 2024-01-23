c88c8f36afcd296f52802ed729824614f778b293
EnDe <viele@public-files.de>
2024-01-23 11:01:20 +0100

    ED: formal changes

--- o-saft-img.tcl
+++ o-saft-img.tcl
@@ -5,11 +5,11 @@
 #? SYNOPSIS
 #?      source o-saft-img.tcl
 #? VERSION                                                                     
-#?      @(#) o-saft-img.tcl 1.9 21/01/14 01:15:39
+#?      @(#) o-saft-img.tcl 3.1 24/01/23 11:01:09
 #? AUTHOR
-#? Copyright (c) 2021, Achim Hoffmann
+#? Copyright (c) 2024, Achim Hoffmann
 #? This  software is licensed under GPLv2. Please see o-saft.pl for details.
-#?      Project Home: https://www.owasp.org/index.php/O-Saft                   
+#?      Project Home: https://owasp.org/www-project-o-saft/
 #?      Repository:   https://github.com/OWASP/O-Saft  
 # -----------------------------------------------------------------------------
 set cfg(IMGSID) {16.09.09}; # initial SID, do not remove
