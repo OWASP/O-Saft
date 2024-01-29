71a3a15d3f9562b3077f490bb4fe08995b5b6381
EnDe <viele@public-files.de>
2024-01-29 11:32:06 +0100

    ED: formal changes

--- lib/error_handler.pm
+++ lib/error_handler.pm
@@ -73,14 +73,14 @@ package error_handler;
 #? ----------------------------------------------------------------------------
 #? constants:
 #? %OERR    :                                     CONSTANTS used for SSLHello: import them using 
-#?                                                'use error_handler qw(:subs %OERR)'
+#?                                                'use error_handler qw(%OERR)'
 #?#############################################################################
 
 use strict;
 use warnings;
 use Carp;
 
-my  $SID_error  = "@(#) error_hander 3.8 24/01/29 10:40:34";
+my  $SID_error  = "@(#) error_handler.pm 3.9 24/01/29 11:05:03";
 our $VERSION    = "24.01.24";
 
 use Exporter qw(import);
@@ -140,26 +140,18 @@ my %err_hash = (
 
 
 #?---------------------------------------------------------------------------------------
-#? sub version ()
-#? prints the official version number of error_handler (yy-mm-dd)
-sub version {
-    local $\ = ""; # no auto '\n' at the end of the line
-    print "error_handler ($VERSION)\n";
-    return;
-} # version
-
+sub version { return $SID_error; }
+#? return the official version number of error_handler (yy-mm-dd)
 
 #?---------------------------------------------------------------------------------------
-#? sub _compile_err_str (;$)
-#? internal sub that compiles a string ($err_str) based on the hash keys of $err_hash
-#? $err_hash{type} should be defined and known. If it isn't the err_string
-#? remarks this lack all other hash keys are suppressed if they do not exist
-#? or are not defined no input variables needed
-#? if the optional variable 'hash_ref' is used, the referenced hash is used instead of the $err_hash
-
 sub _compile_err_str {  ## no critic qw(Subroutines::ProhibitExcessComplexity)
-    my ($arg_ref) = @_;                         # $arg_ref is optional, internal function: no $class!
+    # internal function that compiles a string ($err_str) based on the hash keys
+    # of $err_hash. $err_hash{type} should be defined and known. If it isn't the
+    # err_string remarks this lack all other hash keys are suppressed if they do
+    # not exist or are not defined no input variables needed if the optional
+    # variable $hash_ref is used, the referenced hash is used instead of the $err_hash
 
+    my ($arg_ref) = @_;     # $arg_ref is optional, internal function: no $class!
     unless (defined ($arg_ref) && ($arg_ref)) { # use \$err_hash if $arg_ref is not defined (default)
         $arg_ref = \%err_hash;
     }  elsif ($err_hash{trace}) {
@@ -167,51 +159,47 @@ sub _compile_err_str {  ## no critic qw(Subroutines::ProhibitExcessComplexity)
     }
 
     my $err_str="";
-    $err_str  = $arg_ref->{module}              if ( (exists ($arg_ref->{module}))  && (defined ($arg_ref->{module}))  );
-    $err_str .= "::".$arg_ref->{sub}            if ( (exists ($arg_ref->{sub}))     && (defined ($arg_ref->{sub}))     );
-    $err_str .= " (".$arg_ref->{id}."):"        if ( (exists ($arg_ref->{id}))      && (defined ($arg_ref->{id}))      );
-    $err_str .= " ".$arg_ref->{message}         if ( (exists ($arg_ref->{message})) && (defined ($arg_ref->{message})) );
+    $err_str  = $arg_ref->{module}       if ( (exists ($arg_ref->{module}))  && (defined ($arg_ref->{module}))  );
+    $err_str .= "::".$arg_ref->{sub}     if ( (exists ($arg_ref->{sub}))     && (defined ($arg_ref->{sub}))     );
+    $err_str .= " (".$arg_ref->{id}."):" if ( (exists ($arg_ref->{id}))      && (defined ($arg_ref->{id}))      );
+    $err_str .= " ".$arg_ref->{message}  if ( (exists ($arg_ref->{message})) && (defined ($arg_ref->{message})) );
     if ( (exists ($arg_ref->{type})) && (defined ($arg_ref->{type})) ) {    # type key is used
         # check if is type is known (defind in the reverse hash):
         if ( (exists ($OERR_map{$arg_ref->{type}})) && (defined ($OERR_map{$arg_ref->{type}})) ) {
             if ( (exists ($arg_ref->{trace})) && (0<$arg_ref->{trace}) ) {  # show the type if trace is used
                 $err_str .= " [Type=".$OERR_map{$arg_ref->{type}};
-                $err_str .= "(".$arg_ref->{type}.")"        if (2<$arg_ref->{trace});
+                $err_str .= "(".$arg_ref->{type}.")" if (2<$arg_ref->{trace});
                 $err_str .= "]";
             } # end trace
-        } else {                                                            # unknown type (not defined in %OERR_map)
-            $err_str .= " [Type= ".$OERR{'UNKNOWN_TXT'}." (".$arg_ref->{type}.")]";
+        } else {    # unknown type (not defined in %OERR_map)
+            $err_str .= " [Type= $OERR{'UNKNOWN_TXT'} (".$arg_ref->{type}.")]";
         }
-    } else {                                                                # undefined type
-        $err_str .= " [Type=".$OERR{'UNDEFINED_TXT'}."]";
+    } else {        # undefined type
+        $err_str .= " [Type=$OERR{'UNDEFINED_TXT'}]";
     }
     return ($err_str);
 } # _compile_err_str
 
 
 #?---------------------------------------------------------------------------------------
-#? sub new($$):
-#? set default values of an error hash and set values for received elements
-#? parameters:
-#?   $class:      added automatically when method is used
-#?   $arg_ref:    the referenced hash ovwerwrites the $err_hash if its type is
-#?                more fatal than the old type
 sub new {
+    #? set default values of an error hash and set values for received elements
+    #? parameters:
+    #?   $class:      added automatically when method is used
+    #?   $arg_ref:    the referenced hash ovwerwrites the $err_hash if its type
+    #?                is more fatal than the old type
     my ($class, $arg_ref) = @_;                 # $class is not used
     my $tmp_err_str       = "";
     my $tmp_text          = "";
 
     # error handling inside error handling:
     # undefined/unknown error type in static err_hash
-    #unless ( (exists ($OERR_map{$err_hash{type}})) ) {
     unless ( (exists ($OERR_map{$err_hash{type}})) && (defined ($OERR_map{$err_hash{type}})) ) {
-print "## $err_hash{type} #\n";
         $tmp_err_str = _compile_err_str();
         $tmp_text    = "error_handler::new: internal error: unknown error type in";
         print qq($tmp_text "$tmp_err_str") if ($err_hash{trace});
         carp (qq($tmp_text "$tmp_err_str"));
         $err_hash{type} = $OERR{'UNKNOWN_TYPE'};        # define error type to 'unknown', which is the most fatal
-print "## $OERR_map{$err_hash{type}} #\n"; exit;
     } else {
         # undefined $arg_ref: no new error
         unless (defined ($arg_ref)) {
@@ -253,9 +241,9 @@ print "## $OERR_map{$err_hash{type}} #\n"; exit;
 
 
 #?---------------------------------------------------------------------------------------
-#? reset the error_handler (no error)
-#? opionally owerwrite it with the hash values referenced by arg_ref
 sub reset_err {
+    #? reset the error_handler (no error)
+    #? opionally owerwrite it with the hash values referenced by arg_ref
     my ($class, $arg_ref) = @_;                 # $class is not used
     %err_hash = (                               # reset to default values and overwrite by optional hash arg_ref
         type      => $OERR{'NO_ERROR'},
@@ -281,57 +269,52 @@ sub reset_err {
 
 
 #?---------------------------------------------------------------------------------------
-#? sub is_err():
-#? returns true (1) if an error is stored in the hash of the error_handler
 sub is_err {
+    #? returns true (1) if an error is stored in the hash of the error_handler
     if ( (exists ($err_hash{type})) && (defined ($err_hash{type})) ) {
         return ($err_hash{type} != $OERR{'NO_ERROR'});
-    } else { # internal error: no type defined
-       my $err_str = "error_handler::is_err: internal error: undefined error type in \$error_hash: ";
-       $err_str .= _compile_err_str();
-       print "$err_str\n" if (2<$err_hash{trace});
-       carp ($err_str);
-       return (1);
-   }
+    }
+    # else # internal error: no type defined
+    my $err_str = "error_handler::is_err: internal error: undefined error type in \$error_hash: ";
+    $err_str .= _compile_err_str();
+    print "$err_str\n" if (2<$err_hash{trace});
+    carp ($err_str);
+    return (1);
 } # is_err
 
 
 #?---------------------------------------------------------------------------------------
-#? sub get_err_type():
-#? get error type (number)
 sub get_err_type {
+    #? return error type (number)
     if ( (exists ($err_hash {type})) && (defined ($err_hash {type})) ) {
         return ($err_hash {type});
-    } else {
-        print "Error type is ".$OERR{'UNDEFINED_TXT'} if ($err_hash{trace});
-        carp ("Error type is ".$OERR{'UNDEFINED_TXT'});
     }
+    # else
+    print "Error type is ".$OERR{'UNDEFINED_TXT'} if ($err_hash{trace});
+    carp ("Error type is ".$OERR{'UNDEFINED_TXT'});
     return (undef);
 } # get_err_type
 
 
 #?---------------------------------------------------------------------------------------
-#? sub get_err_type_name():
-#? get error type name
 sub get_err_type_name {
+    #? return error type name
     if ( (exists ($err_hash {type})) && (defined ($err_hash {type})) ) {
         return ($OERR_map{$err_hash{type}}) if ( (exists ($OERR_map{$err_hash{type}})) && (defined ($OERR_map{$err_hash{type}})) );
-        return ($OERR{'UNKNOWN_TXT'});
-    } else {
-        print "Error type is ".$OERR{'UNDEFINED_TXT'} if ($err_hash{trace});
-        carp ("Error type is ".$OERR{'UNDEFINED_TXT'});
     }
+    # else
+    print "Error type is ".$OERR{'UNDEFINED_TXT'} if ($err_hash{trace});
+    carp ("Error type is ".$OERR{'UNDEFINED_TXT'});
     return ($OERR{'UNDEFINED_TXT'});
 } # get_err_type_name
 
 
 #?---------------------------------------------------------------------------------------
-#? sub get_err_val():
-#? get a single value of an error hash element
-#? parameters:
-#?   $class:      added automatically when method is used
-#?   $key_arg:    hash key where the value sould be fetched
 sub get_err_val {
+    #? return a single value of an error hash element
+    #? parameters:
+    #?   $class:      added automatically when method is used
+    #?   $key_arg:    hash key where the value sould be fetched
     my ($class, $key_arg) = @_;                 # $class is not used
     return ($err_hash {$key_arg}) if ( (exists ($err_hash {$key_arg})) && (defined ($err_hash {$key_arg})) );
     return;
@@ -339,10 +322,9 @@ sub get_err_val {
 
 
 #?---------------------------------------------------------------------------------------
-#? sub get_err_str():
-#? get the error string
-#? no input variable needed
 sub get_err_str {
+    #? return the error string
+    #? no input variable needed
     unless ( (exists ($OERR_map{$err_hash{type}})) && (defined ($OERR_map{$err_hash{type}})) ) { # undefined Error Type
         my $tmp_err_str = _compile_err_str();
         my$tmp_text     = "error_handler::get_err_str: internal error: unknown error type in";
@@ -355,14 +337,13 @@ sub get_err_str {
 
 
 #?---------------------------------------------------------------------------------------
-#? sub get_err_hash ($;$$):
-#? get the error hash as string (mainly used for debugging)
-#? parameters:
-#?   $class:      added automatically when method is used
-#?   $prefix:     optional prefix after new line (e.g. some spaces for the indent)
-#?   $hash_ref:   optional ref to an error_hash (default: %err_hash)
-#? returns the compiled output
 sub get_err_hash {
+    #? return the error hash as string (mainly used for debugging)
+    #? parameters:
+    #?   $class:      added automatically when method is used
+    #?   $prefix:     optional prefix after new line (e.g. some spaces for the indent)
+    #?   $hash_ref:   optional ref to an error_hash (default: %err_hash)
+    #? returns the compiled output
     my ($class, $prefix, $hash_ref) = @_;           # $class is not used later, it is added automatically when calling the method
     my $err_hash_str                = "";
     $prefix =   ""         if (not defined($prefix));   # default is no indent
@@ -379,13 +360,12 @@ sub get_err_hash {
 
 
 #?---------------------------------------------------------------------------------------
-#? sub get_all_err_types($;$)
-#? get all possible defined error types and their internal representation as
-#? a string (mainly used for debugging)
-#? parameters:
-#?   $class:      added automatically when method is used
-#?   $prefix:     optional prefix after new line (e.g. some spaces for the indent)
 sub get_all_err_types {
+    #? return all possible defined error types and their internal representation
+    #? as a string (mainly used for debugging)
+    #? parameters:
+    #?   $class:      added automatically when method is used
+    #?   $prefix:     optional prefix after new line (e.g. some spaces for the indent)
     my ($class, $prefix) = @_;
     my $err_types_str="";
     print ">get_all_err_types\n" if ($err_hash{trace});
