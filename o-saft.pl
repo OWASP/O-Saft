828a822e8b967ae35e377b0478117ab56548011a
EnDe <viele@public-files.de>
2024-04-27 01:13:17 +0200

    EF: don't read any file if file from OSAFT_CONFIG does not exist

--- o-saft.pl
+++ o-saft.pl
@@ -69,7 +69,7 @@ use warnings;
 no warnings 'once';     ## no critic qw(TestingAndDebugging::ProhibitNoWarnings)
    # "... used only once: possible typo ..." appears when OTrace.pm not included
 
-our $SID_main   = "@(#) yeast.pl 3.43 24/04/27 01:09:03"; # version of this file
+our $SID_main   = "@(#) yeast.pl 3.44 24/04/27 01:13:10"; # version of this file
 my  $VERSION    = _VERSION();           ## no critic qw(ValuesAndExpressions::RequireConstantVersion)
     # SEE Perl:constant
     # see _VERSION() below for our official version number
@@ -414,7 +414,7 @@ our %check_http = %OData::check_http;
 our %check_size = %OData::check_size;
 
 $cfg{'time0'}   = $time0;
-OCfg::set_user_agent("$cfg{'me'}/3.43"); # use version of this file not $VERSION
+OCfg::set_user_agent("$cfg{'me'}/3.44"); # use version of this file not $VERSION
 OCfg::set_user_agent("$cfg{'me'}/$STR{'MAKEVAL'}") if (defined $ENV{'OSAFT_MAKE'});
 # TODO: $STR{'MAKEVAL'} is wrong if not called by internal make targets
 
@@ -598,9 +598,10 @@ if (0 < _is_argv('(?:--rc)')) {                 # (re-)compute default RC-File w
 }
 if (defined $ENV{'OSAFT_CONFIG'}) {
     if (-e $ENV{'OSAFT_CONFIG'}) {
-        $cfg{'RC-FILE'} =  $ENV{'OSAFT_CONFIG'};
+        $cfg{'RC-FILE'} = $ENV{'OSAFT_CONFIG'};
     } else {
-        _warn("039: OSAFT_CONFIG '$ENV{'OSAFT_CONFIG'}' does not exit; ignored");
+        $cfg{'RC-FILE'} = "";   # don't read default file
+        _warn("039: OSAFT_CONFIG '$ENV{'OSAFT_CONFIG'}' does not exit; no RC-FILE read");
     }
 }
 if (0 < _is_argv('(?:--rc=)')) {                # other RC-FILE given
