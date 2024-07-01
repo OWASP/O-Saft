9838880db2655b92748c934d2438f5d789eea8ba
EnDe <viele@public-files.de>
2024-07-01 08:29:30 +0200

    ED: formal change

--- o-saft.pl
+++ o-saft.pl
@@ -69,7 +69,7 @@ use warnings;
 no warnings 'once';     ## no critic qw(TestingAndDebugging::ProhibitNoWarnings)
    # "... used only once: possible typo ..." appears when OTrace.pm not included
 
-our $SID_main   = "@(#) yeast.pl 3.75 24/06/26 09:59:47"; # version of this file
+our $SID_main   = "@(#) o-saft.pl 3.76 24/07/01 08:27:55"; # version of this file
 my  $VERSION    = _VERSION();           ## no critic qw(ValuesAndExpressions::RequireConstantVersion)
     # SEE Perl:constant
     # see _VERSION() below for our official version number
@@ -418,7 +418,7 @@ our %check_http = %OData::check_http;
 our %check_size = %OData::check_size;
 
 $cfg{'time0'}   = $time0;
-OCfg::set_user_agent("$cfg{'me'}/3.75"); # use version of this file not $VERSION
+OCfg::set_user_agent("$cfg{'me'}/3.76"); # use version of this file not $VERSION
 OCfg::set_user_agent("$cfg{'me'}/$STR{'MAKEVAL'}") if (defined $ENV{'OSAFT_MAKE'});
 # TODO: $STR{'MAKEVAL'} is wrong if not called by internal make targets
 
