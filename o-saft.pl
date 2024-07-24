912edba4403681b6f4a8f3d2d1da9e9bf8955ca0
EnDe <viele@public-files.de>
2024-07-24 16:03:16 +0200

    BF: avoid "Undefined subroutine &SSLinfo::do_ssl_open ..." for some cipher check commands like +cbs (issue 140)

--- o-saft.pl
+++ o-saft.pl
@@ -69,7 +69,7 @@ use warnings;
 no warnings 'once';     ## no critic qw(TestingAndDebugging::ProhibitNoWarnings)
    # "... used only once: possible typo ..." appears when OTrace.pm not included
 
-our $SID_main   = "@(#) o-saft.pl 3.85 24/07/18 21:54:21"; # version of this file
+our $SID_main   = "@(#) o-saft.pl 3.86 24/07/24 16:03:00"; # version of this file
 my  $VERSION    = _VERSION();           ## no critic qw(ValuesAndExpressions::RequireConstantVersion)
     # SEE Perl:constant
     # see _VERSION() below for our official version number
@@ -418,7 +418,7 @@ our %check_http = %OData::check_http;
 our %check_size = %OData::check_size;
 
 $cfg{'time0'}   = $time0;
-OCfg::set_user_agent("$cfg{'me'}/3.85"); # use version of this file not $VERSION
+OCfg::set_user_agent("$cfg{'me'}/3.86"); # use version of this file not $VERSION
 OCfg::set_user_agent("$cfg{'me'}/$STR{'MAKEVAL'}") if (defined $ENV{'OSAFT_MAKE'});
 # TODO: $STR{'MAKEVAL'} is wrong if not called by internal make targets
 
@@ -6109,7 +6109,7 @@ sub printversion        {
     my $me = $cfg{'me'};
     print( "= $0 " . _VERSION() . " =");
     if (not _is_cfg_verbose()) {
-        printf("    %-21s%s\n", $me, "3.85");# just version to keep make targets happy
+        printf("    %-21s%s\n", $me, "3.86");# just version to keep make targets happy
     } else {
         printf("    %-21s%s\n", $me, $SID_main); # own unique SID
         # print internal SID of our own modules
@@ -7560,7 +7560,7 @@ _trace_info("  LOAD0   - load modules start");
 
 #| import common and private modules
 #| -------------------------------------
-if (1 > _need_netinfo() and not $test) {
+if (1 > (_need_netinfo()  + _need_checkssl()) and not $test) {
     # SEE Note:need SSLinfo
     $cfg{'need_netinfo'} = 0 if _is_cfg_ciphermode('intern');
     # TODO: following necessary for _get_data0(), if called as single command
