9dd6b71ef3f6995d650b885ae8395a89ec8158d6
EnDe <viele@public-files.de>
2024-08-29 12:31:34 +0200

    BF: _check_openssl() improved (bug in 3.135 only)

--- o-saft.pl
+++ o-saft.pl
@@ -65,7 +65,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $SID_main   = "@(#) o-saft.pl 3.135 24/08/29 11:44:17"; # version of this file
+our $SID_main   = "@(#) o-saft.pl 3.136 24/08/29 12:03:04"; # version of this file
 my  $VERSION    = _VERSION();           ## no critic qw(ValuesAndExpressions::RequireConstantVersion)
     # SEE Perl:constant
     # see _VERSION() below for our official version number
@@ -412,7 +412,7 @@ our %cmd = (
 ); # %cmd
 
 $cfg{'time0'}   = $time0;
-OCfg::set_user_agent("$cfg{'me'}/3.135"); # use version of this file not $VERSION
+OCfg::set_user_agent("$cfg{'me'}/3.136"); # use version of this file not $VERSION
 OCfg::set_user_agent("$cfg{'me'}/$STR{'MAKEVAL'}") if (defined $ENV{'OSAFT_MAKE'});
 # TODO: $STR{'MAKEVAL'} is wrong if not called by internal make targets
 
@@ -1727,6 +1727,11 @@ sub _check_openssl  {
     $SSLinfo::trace   = $cfg{'trace'};
         # safe to set $SSLinfo::* here,
         # will be redifined later, see: set defaults for SSLinfo
+    if (not defined &SSLinfo::s_client_check) {
+        # SSLinfo.pm not loaded, no need to perform checks
+        trace("_check_openssl: SSLinfo.pm not loaded, no checks performed");
+        return;
+    }
     if (not defined SSLinfo::s_client_check()) {
         _warn("147: '$cmd{'openssl'}' not available; all openssl functionality disabled");
         _hint("consider using '--openssl=/path/to/openssl'");
@@ -6140,7 +6145,7 @@ sub printversion        {
     my $me = $cfg{'me'};
     print( "= $0 " . _VERSION() . " =");
     if (not _is_cfg_verbose()) {
-        printf("    %-21s%s\n", $me, "3.135");# just version to keep make targets happy
+        printf("    %-21s%s\n", $me, "3.136");# just version to keep make targets happy
     } else {
         printf("    %-21s%s\n", $me, $SID_main); # own unique SID
         # print internal SID of our own modules
