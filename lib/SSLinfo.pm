abacf1466155ff28fe256aaadc7575c7d8ff5642
EnDe <viele@public-files.de>
2024-09-04 00:19:01 +0200

    EF: _warn() -> OCfg::warn()

--- lib/SSLinfo.pm
+++ lib/SSLinfo.pm
@@ -49,7 +49,7 @@ use warnings;
 #_____________________________________________________________________________
 #___________________________________________________ package initialisation __|
 
-my  $SID_sslinfo    =  "@(#) SSLinfo.pm 3.28 24/09/01 12:02:17";
+my  $SID_sslinfo    =  "@(#) SSLinfo.pm 3.29 24/09/04 00:18:49";
 our $VERSION        =  "24.06.24";  # official verion number of this file
 
 BEGIN {
@@ -160,8 +160,8 @@ BEGIN {
         sub _trace1 { my @txt=@_; __trace(@txt) if (1 == $SSLinfo::trace); return; }
         sub _trace2 { my @txt=@_; __trace(@txt) if (1  < $SSLinfo::trace); return; }
     }
-    if (not exists &_warn) {
-        sub _warn   { my @txt=@_; printf("%s%s\n", ($STR{'WARN'}||"**wARNING: "), "@txt"); return; }
+    if (not exists &OCfg::warn) {
+        sub OCfg::warn   { my @txt=@_; printf("%s%s\n", ($STR{'WARN'}||"**wARNING: "), "@txt"); return; }
     }
     if (not exists &_dbx) {
         sub _dbx    { my @txt=@_; printf("%s%s\n", ($STR{'DBX'} ||"#dbx# "), "@txt"); return; }
