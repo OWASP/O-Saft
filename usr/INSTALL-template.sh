f9b9577d141edaf9e2fe9ec24d2dc034d50d037b
EnDe <viele@public-files.de>
2025-03-01 16:28:05 +0100

    ED: formal changes

--- usr/INSTALL-template.sh
+++ usr/INSTALL-template.sh
@@ -308,7 +308,7 @@
 #?      # check SIDs and checksums of all installed files:
 #?          $0 . --check=SID --changes
 #?      - should return an empty list like:
-#?          # ./INSTALL.sh 3.65; --check=SID  . ...
+#?          # ./INSTALL.sh 3.66; --check=SID  . ...
 #?
 #?          # SID   date    time    md5sum   filename    path
 #?          #----------------------+--------+-------------------------------
@@ -409,7 +409,7 @@
 
 #_____________________________________________________________________________
 #_____________________________________________ internal variables; defaults __|
-SID="@(#) INSTALL-template.sh 3.65 25/03/01 15:59:37"
+SID="@(#) INSTALL-template.sh 3.66 25/03/01 16:27:45"
 try=''
 ich=${0##*/}
 dir=${0%/*}
@@ -1498,8 +1498,8 @@ while [ $# -gt 0 ]; do
 		\sed -ne '/^#? VERSION/{' -e n -e 's/#?//' -e p -e '}' $0
 		exit 0
 		;;
-	  '+VERSION')   echo 3.65 ; exit;        ;; # for compatibility to $osaft_exe
-	  3.65 | 3* | 4*) ;; # ignore version number
+	  '+VERSION')   echo 3.66 ; exit;        ;; # for compatibility to $osaft_exe
+	  3.66 | 3* | 4*) ;; # ignore version number
 	  *)            new_dir="$1"   ;        ;; # directory, last one wins
 	esac
 	shift
@@ -1535,7 +1535,7 @@ clean_directory="$inst_directory/$clean_directory"
 src_txt=
 [ "install" = "$mode" ] && src_txt="$src_directory -->"
 echo   "$0 $optn $force $_break $ignore $other $changes $noargs $useenv $gnuenv $instdev $inst_directory"
-echo_info "# $0 3.65 $mode $src_txt $inst_directory "
+echo_info "$0 3.66 $mode $src_txt $inst_directory "
     # always print internal SID, makes debugging simpler
 
 _b='`'  # using backticks in echo is tricky ...
