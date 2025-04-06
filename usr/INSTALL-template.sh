239eb7bb451a3b9f36c5f9230bfb453f8a754c68
EnDe <viele@public-files.de>
2025-04-07 00:47:00 +0200

    BF: generate missing files

--- usr/INSTALL-template.sh
+++ usr/INSTALL-template.sh
@@ -308,7 +308,7 @@
 #?      # check SIDs and checksums of all installed files:
 #?          $0 . --check=SID --changes
 #?      - should return an empty list like:
-#?          # ./INSTALL.sh 3.73; --check=SID  . ...
+#?          # ./INSTALL.sh 3.74; --check=SID  . ...
 #?
 #?          # SID   date    time    md5sum   filename    path
 #?          #----------------------+--------+-------------------------------
@@ -409,7 +409,7 @@
 
 #_____________________________________________________________________________
 #_____________________________________________ internal variables; defaults __|
-SID="@(#) INSTALL-template.sh 3.73 25/04/07 00:05:30"
+SID="@(#) INSTALL-template.sh 3.74 25/04/07 00:46:42"
 try=''
 ich=${0##*/}
 dir=${0%/*}
@@ -470,6 +470,7 @@ osaft_modules="
 	INSERTED_BY_MAKE_OSAFT_MODULES
 "
 
+file_cgi_html="INSERTED_BY_MAKE_OSAFT_CGI_HTML"
 files_contrib="
 	INSERTED_BY_MAKE_CONTRIB
 "
@@ -1509,8 +1510,8 @@ while [ $# -gt 0 ]; do
 		\sed -ne '/^#? VERSION/{' -e n -e 's/#?//' -e p -e '}' $0
 		exit 0
 		;;
-	  '+VERSION')   echo 3.73 ; exit;        ;; # for compatibility to $osaft_exe
-	  3.73 | 3* | 4*) ;; # ignore version number
+	  '+VERSION')   echo 3.74 ; exit;        ;; # for compatibility to $osaft_exe
+	  3.74 | 3* | 4*) ;; # ignore version number
 	  *)            new_dir="$1"   ;        ;; # directory, last one wins
 	esac
 	shift
@@ -1546,7 +1547,7 @@ clean_directory="$inst_directory/$clean_directory"
 src_txt=
 [ "install" = "$mode" ] && src_txt="$src_directory -->"
 echo   "$0 $optn $force $_break $ignore $other $changes $noargs $useenv $gnuenv $instdev $inst_directory"
-echo_info "$0 3.73 $mode $src_txt $inst_directory "
+echo_info "$0 3.74 $mode $src_txt $inst_directory "
     # always print internal SID, makes debugging simpler
 
 _b='`'  # using backticks in echo is tricky ...
@@ -1577,6 +1578,12 @@ fi
 _error="**ERROR: 006: can't cd to '$inst_directory'; --n enforced"
 # Note: --instdev is not a mode, just an option for --install, hence
 #       handled in mode_install()
+
+# generate missing files (quick&dirty, needed for --install, --cgi only)
+if [ -e "$osaft_exe" ]; then
+	[ -e "$file_cgi_html" ] \
+	|| $osaft_exe --no-rc --no-warning --help=gen-cgi  > "$file_cgi_html"
+fi
 case $mode in
 	usage)      mode_usage   ; ;;
 	checkdev)   mode_checkdev; ;;
